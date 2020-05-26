6d20e26bf791717dd2050a75b4494a69b2e508d8
Thomas DiCarlo <tomdicarlo@hotmail.com>
2020-05-26 17:19:51 -0400

    Component showcases (#4)
    * Added Shadow Study to  Sample Showcase
    
    * Progress on Button Showcase
    
    * All Components now have an individual page
    
    * UI-Button Sample Cleaned Up
    
    Co-authored-by: Thomas M. Dicarlo - Intern <Thomas.Dicarlo@bentley.com>

--- src/Components/SampleShowcase/SampleShowcase.tsx
+++ src/Components/SampleShowcase/SampleShowcase.tsx
@@ -14,9 +14,25 @@ import { getHeatmapDecoratorSpec } from "../../frontend-samples/heatmap-decorato
 import { getMarkerPinSpec } from "../../frontend-samples/marker-pin-sample";
 import { getTooltipCustomizeSpec } from "../../frontend-samples/tooltip-customize-sample";
 import { getShadowStudySpec } from "../../frontend-samples/shadow-study-sample";
+import { getViewerOnly2dSpec } from "../../frontend-samples/viewer-only-2d-sample";
+
+import { getButtonSpec } from "../../frontend-samples/component-gallery/button-sample";
+//import { getBadgeSpec } from "../../frontend-samples/component-gallery/badge-sample";
+//import { getCheckListBoxSpec } from "../../frontend-samples/component-gallery/checklistbox-sample";
+//import { getContextMenuSpec } from "../../frontend-samples/component-gallery/context-menu-sample";
+//import { getExpandableListSpec } from "../../frontend-samples/component-gallery/expandable-list-sample";
+//import { getInputsSpec } from "../../frontend-samples/component-gallery/inputs-sample";
+//import { getLoadingSpec } from "../../frontend-samples/component-gallery/loading-sample";
+//import { getSearchBoxSpec } from "../../frontend-samples/component-gallery/search-box-sample";
+//import { getSliderSpec } from "../../frontend-samples/component-gallery/slider-sample";
+//import { getSplitButtonSpec } from "../../frontend-samples/component-gallery/split-button-sample";
+//import { getTabsSpec } from "../../frontend-samples/component-gallery/tabs-sample";
+//import { getTextSpec } from "../../frontend-samples/component-gallery/text-sample";
+//import { getTilesSpec } from "../../frontend-samples/component-gallery/tiles-sample";
+//import { getToggleSpec } from "../../frontend-samples/component-gallery/toggle-sample";
+
 import { getViewAttributesSpec } from "../../frontend-samples/view-attributes-sample";
 import { getViewClipSpec } from "../../frontend-samples/view-clip-sample";
-import { getViewerOnly2dSpec } from "../../frontend-samples/viewer-only-2d-sample";
 import { getZoomToElementsSpec } from "../../frontend-samples/zoom-to-elements-sample";
 import { IModelSelector, SampleIModels } from "../IModelSelector/IModelSelector";
 
@@ -53,6 +69,21 @@ export class SampleShowcase extends React.Component<{}, ShowcaseState> {
         this._samples.push(getViewClipSpec());
         this._samples.push(getViewerOnly2dSpec());
         this._samples.push(getZoomToElementsSpec());
+        //this._samples.push(getShadowStudySpec());
+        //this._samples.push(getBadgeSpec());
+        this._samples.push(getButtonSpec());
+        //this._samples.push(getCheckListBoxSpec());
+        ////this._samples.push(getContextMenuSpec());
+        //this._samples.push(getExpandableListSpec());
+        //this._samples.push(getInputsSpec());
+        //this._samples.push(getLoadingSpec());
+        //this._samples.push(getSearchBoxSpec());
+        //this._samples.push(getSliderSpec());
+        //this._samples.push(getSplitButtonSpec());
+        //this._samples.push(getTabsSpec());
+        //this._samples.push(getTextSpec());
+        //this._samples.push(getTilesSpec());
+        //this._samples.push(getToggleSpec());
 
         this.state = {
             iModelName: SampleIModels.RetailBuilding
---
+++ src/frontend-samples/component-gallery/checklistbox-sample/index.tsx
@@ -0,0 +1,73 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import * as React from "react";
+import "@bentley/icons-generic-webfont/dist/bentley-icons-generic-webfont.css";
+import { SampleSpec } from "../../../Components/SampleShowcase/SampleShowcase";
+import { GithubLink } from "../../../Components/GithubLink";
+import "../../../common/samples-common.scss";
+
+import "../CommonComponentTools/index.scss";
+import {ComponentContainer, ComponentExampleProps} from "../CommonComponentTools/ComponentContainer";
+
+import { SampleIModels } from "../../../Components/IModelSelector/IModelSelector";
+import { CheckListBox, CheckListBoxItem, CheckListBoxSeparator } from "@bentley/ui-core"
+
+
+
+export function getCheckListBoxSpec(): SampleSpec {
+  return ({
+    name: "checklistbox-sample",
+    label: "CheckListBox",
+    image: "viewport-only-thumbnail.png",
+    setup: CheckListBoxList.setup ,
+  });
+}
+
+
+export const createComponentExample = (title: string, description: string | undefined, content: React.ReactNode): ComponentExampleProps => {
+    return { title, description, content };
+};
+
+
+
+export class CheckListBoxList extends React.Component<{}> {
+
+    public static getCheckListBoxData(): ComponentExampleProps[] {
+        return [
+            createComponentExample("CheckListBox", undefined,
+              <CheckListBox>
+                <CheckListBoxItem label="Item 1" />
+                <CheckListBoxItem label="Item 2" />
+              </CheckListBox>),
+            createComponentExample("CheckListBox with separator", undefined,
+              <CheckListBox>
+                <CheckListBoxItem label="Item 1" />
+                <CheckListBoxItem label="Item 2" />
+                <CheckListBoxSeparator />
+                <CheckListBoxItem label="Item 3" />
+                <CheckListBoxItem label="Item 4" />
+              </CheckListBox>),
+          ]
+    }
+
+    public static async setup() {
+        return <CheckListBoxList></CheckListBoxList>
+    }
+
+    public render() {
+        return (
+            <>
+            <div className="sample-ui">
+                <div>
+                <span>Different Styles of Buttons</span>
+                <GithubLink linkTarget="https://github.com/imodeljs/imodeljs-samples/tree/master/frontend-samples/viewer-only-sample" />
+                    <ComponentContainer data = {CheckListBoxList.getCheckListBoxData()}></ComponentContainer>
+                </div>
+            </div>
+            </>
+        );
+    }
+  
+}
