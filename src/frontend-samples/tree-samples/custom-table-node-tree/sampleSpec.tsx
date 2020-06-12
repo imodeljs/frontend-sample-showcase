a15e8fb15b55f71cbdb5ab23d6532b157091a2f5
tomdicarlo <24437642+tomdicarlo@users.noreply.github.com>
2020-06-12 15:51:16 -0400

    Fixed file list

--- src/frontend-samples/tree-samples/custom-table-node-tree/sampleSpec.tsx
+++ src/frontend-samples/tree-samples/custom-table-node-tree/sampleSpec.tsx
@@ -8,8 +8,7 @@ export function getCustomTableNodeTreeSpec(): SampleSpec {
     image: "custom-table-node-tree-thumbnail.png",
     customModelList: [],
     files: [
-      { name: "CustomTableNodeTreeSample.tsx", import: import("!!raw-loader!./index") },
-      { name: "Common.tsx", import: import("!!raw-loader!../Common") },
+      { name: "CustomTableNodeTree.tsx", import: import("!!raw-loader!./index") },
       { name: "index.scss", import: import("!!raw-loader!../index.scss") },
       { name: "TableNodeTree.scss", import: import("!!raw-loader!./TableNodeTree.scss") },
       { name: "GithubLink.tsx", import: import("!!raw-loader!../../../Components/GithubLink") },
