9a2129f264f70f0b620805cb6528852dfacf51ac
Will Bentley <williamkbentley@gmail.com>
2020-07-29 09:39:50 -0400

    Appease linter

--- src/frontend-samples/view-clip-sample/ViewClipApp.tsx
+++ src/frontend-samples/view-clip-sample/ViewClipApp.tsx
@@ -6,7 +6,7 @@ import * as React from "react";
 import "@bentley/icons-generic-webfont/dist/bentley-icons-generic-webfont.css";
 import "common/samples-common.scss";
 import { ViewClipUI } from "./ViewClipUI";
-import { EditManipulator, IModelApp, IModelConnection, ScreenViewport, ViewClipClearTool, ViewClipDecorationProvider, Viewport, ViewClipTool } from "@bentley/imodeljs-frontend";
+import { EditManipulator, IModelApp, IModelConnection, ScreenViewport, ViewClipClearTool, ViewClipDecorationProvider, ViewClipTool, Viewport } from "@bentley/imodeljs-frontend";
 import { ClipMaskXYZRangePlanes, ClipPlane, ClipPrimitive, ClipShape, ClipVector, ConvexClipPlaneSet, Plane3dByOriginAndUnitNormal, Point3d, Vector3d } from "@bentley/geometry-core";
 import SampleApp from "common/SampleApp";
 
