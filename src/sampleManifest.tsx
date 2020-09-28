dba6c5bc01d65c8e49c78e7922d2346962700c5f
Linas Burneika <Linas.Burneika@bentley.com>
2020-09-28 16:34:31 +0300

    Sort imports

--- src/sampleManifest.tsx
+++ src/sampleManifest.tsx
@@ -8,6 +8,7 @@ import { getViewportOnlySpec } from "./frontend-samples/viewport-only-sample/sam
 import { getDisplayStylesSpec } from "frontend-samples/display-styles-sample/sampleSpec";
 import { getEmphasizeElementsSpec } from "./frontend-samples/emphasize-elements-sample/sampleSpec";
 import { getHeatmapDecoratorSpec } from "./frontend-samples/heatmap-decorator-sample/sampleSpec";
+import { getImageExportSpec } from "frontend-samples/image-export/sampleSpec";
 import { getMarkerPinSpec } from "./frontend-samples/marker-pin-sample/sampleSpec";
 import { getMultiViewportSpec } from "frontend-samples/multi-viewport-sample/sampleSpec";
 import { getShadowStudySpec } from "./frontend-samples/shadow-study-sample/sampleSpec";
@@ -44,7 +45,7 @@ import { getUnifiedSelectionTreeSpec } from "./frontend-samples/tree-samples/uni
 import { getPresentationTreeSpec } from "./frontend-samples/tree-samples/presentation-tree/sampleSpec";
 import { getToolbarButtonSample } from "./frontend-samples/app-ui-samples/toolbar-button-provider-sample/sampleSpec";
 import { getViewportFrontstageSample } from "./frontend-samples/app-ui-samples/viewport-frontstage-sample/sampleSpec";
-import { getImageExportSpec } from "frontend-samples/image-export/sampleSpec";
+
 
 export interface SampleSpecGroup {
   groupName: string;
