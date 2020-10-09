d572295b2d1dec148081a0f933a4b8d80f7db0de
tomdicarlo <24437642+tomdicarlo@users.noreply.github.com>
2020-10-09 12:35:15 -0400

    Can change line segment points in simple line sample

--- src/frontend-samples/geometry-samples/simple-line-sample/SimpleLine.tsx
+++ src/frontend-samples/geometry-samples/simple-line-sample/SimpleLine.tsx
@@ -9,11 +9,31 @@ import { LineSegment3d, Point3d } from "@bentley/geometry-core";
 import { GeometryDecorator } from "common/GeometryCommon/GeometryDecorator";
 import { IModelApp } from "@bentley/imodeljs-frontend";
 import { ControlPane } from "Components/ControlPane/ControlPane";
-export default class SimpleLine extends React.Component<{}, {}> implements SampleApp {
+import { NumericInput } from "@bentley/ui-core";
+
+interface SimpleLineState {
+  point1X: number;
+  point1Y: number;
+  point2X: number;
+  point2Y: number;
+}
+
+export default class SimpleLine extends React.Component<{}, SimpleLineState> implements SampleApp {
+
+  constructor(props?: any, context?: any) {
+    super(props, context);
+    this.state = {
+      point1X: 140,
+      point1Y: 25,
+      point2X: 680,
+      point2Y: 800,
+    };
+  }
+
 
   public static async setup(): Promise<React.ReactNode> {
     await BlankViewport.setup();
-    BlankViewport.decorator = new GeometryDecorator();
+    BlankViewport.decorator = new GeometryDecorator(true, 10);
     IModelApp.viewManager.addDecorator(BlankViewport.decorator);
     return <SimpleLine></SimpleLine>;
   }
@@ -24,10 +44,27 @@ export default class SimpleLine extends React.Component<{}, {}> implements Sampl
     }
   }
 
+  public getControls() {
+    return (
+      <>
+        <div className="sample-options-2col">
+          <span>Point 1 X:</span>
+          <NumericInput defaultValue={this.state.point1X} min={0} max={1000} onChange={(value) => { if (value) this.setState({ point1X: value }); }}></NumericInput>
+          <span>Point 1 Y:</span>
+          <NumericInput defaultValue={this.state.point1Y} min={0} max={1000} onChange={(value) => { if (value) this.setState({ point1Y: value }); }}></NumericInput>
+          <span>Point 2 X:</span>
+          <NumericInput defaultValue={this.state.point2X} min={0} max={1000} onChange={(value) => { if (value) this.setState({ point2X: value }); }}></NumericInput>
+          <span>Point 2 Y:</span>
+          <NumericInput defaultValue={this.state.point2Y} min={0} max={1000} onChange={(value) => { if (value) this.setState({ point2Y: value }); }}></NumericInput>
+        </div>
+      </>
+    );
+  }
+
   public render() {
     return (
       <>
-        <ControlPane instructions="Creating a line segments and some points along it"></ControlPane>
+        <ControlPane instructions="Creating a line segments and some points along it" controls={this.getControls()}></ControlPane>
         <BlankViewport force2d={true}></BlankViewport>
       </>
     );
@@ -37,9 +74,15 @@ export default class SimpleLine extends React.Component<{}, {}> implements Sampl
     this.setGeometry();
   }
 
+  public componentDidUpdate() {
+    this.setGeometry();
+    console.log("updated")
+  }
+
   public setGeometry() {
-    const pointA = Point3d.create(140, 25, 0);
-    const pointB = Point3d.create(680, 800, 0);
+    BlankViewport.decorator.clearGeometry();
+    const pointA = Point3d.create(this.state.point1X, this.state.point1Y, 0);
+    const pointB = Point3d.create(this.state.point2X, this.state.point2Y, 0);
     const myLine = LineSegment3d.create(pointA, pointB);
     BlankViewport.decorator.addGeometry(myLine);
 
