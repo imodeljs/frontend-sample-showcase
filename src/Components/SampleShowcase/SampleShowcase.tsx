098d50721a0d6b3c3196bb174c893611b3988f17
Josh Schifter <josh.schifter@bentley.com>
2020-12-21 09:01:55 -0500

    appease the linter

--- src/Components/SampleShowcase/SampleShowcase.tsx
+++ src/Components/SampleShowcase/SampleShowcase.tsx
@@ -8,7 +8,7 @@ import { SampleGallery } from "../SampleGallery/SampleGallery";
 import "./SampleShowcase.scss";
 import "common/samples-common.scss";
 import { sampleManifest } from "../../sampleManifest";
-import { IModelSelector, SampleIModels } from "../IModelSelector/IModelSelector";
+import { IModelSelector } from "../IModelSelector/IModelSelector";
 import { ConnectedSampleEditor } from "../SampleEditor/SampleEditor";
 import { editorCommonActionContext, IInternalFile, SplitScreen } from "@bentley/monaco-editor/editor";
 import { Button, ButtonSize, ButtonType } from "@bentley/ui-core";
