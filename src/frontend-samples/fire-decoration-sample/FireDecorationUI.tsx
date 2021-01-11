447d3a6bf80da640b342924f00f3d2603dc764e5
30239292+Ellord207@users.noreply.github.com <30239292+Ellord207@users.noreply.github.com>
2021-01-11 10:12:33 -0500

    WIP: particle-ts snow

--- src/frontend-samples/fire-decoration-sample/FireDecorationUI.tsx
+++ src/frontend-samples/fire-decoration-sample/FireDecorationUI.tsx
@@ -2,22 +2,42 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { Point3d } from "@bentley/geometry-core";
 import "@bentley/icons-generic-webfont/dist/bentley-icons-generic-webfont.css";
-import { IModelApp, IModelConnection, Viewport, ViewState } from "@bentley/imodeljs-frontend";
-import { Button } from "@bentley/ui-core";
+import { DisplayStyle3dSettingsProps } from "@bentley/imodeljs-common";
+import { IModelApp, IModelConnection, ScreenViewport, ViewState } from "@bentley/imodeljs-frontend";
+import { Button, Select } from "@bentley/ui-core";
 import { ViewSetup } from "api/viewSetup";
 import "common/samples-common.scss";
 import { ControlPane } from "Components/ControlPane/ControlPane";
 import { ReloadableViewport } from "Components/Viewport/ReloadableViewport";
-import { PlaceMarkerTool } from "frontend-samples/marker-pin-sample/PlaceMarkerTool";
 import * as React from "react";
-import { FireDecorator, FireDecoratorPoint, FireDecoratorSource } from "./FireDecorator";
+import Particles from "react-tsparticles";
+import { FireDecorator, FireDecoratorPoint } from "./FireDecorator";
 
 // cSpell:ignore imodels
 /** The React state for this UI component */
 interface FireDecorationState {
-  vp?: Viewport;
+  screenRectangle?: ClientRect;
+  effect: EffectTypes;
+}
+
+const snowStyle = {
+  environment: {
+    sky: { display: true, twoColor: false, groundColor: 9741199, nadirColor: 5464143, skyColor: 16764303, zenithColor: 0xD3D3D3 },
+    ground: { display: false },
+  },
+};
+const rainStyle = {
+  environment: {
+    sky: { display: true, twoColor: false, groundColor: 9741199, nadirColor: 5464143, skyColor: 0xF2C082, zenithColor: 0xC0C0C0 },
+    ground: { display: false },
+  },
+};
+const effects: any[] = [];
+const styles: DisplayStyle3dSettingsProps[] = [snowStyle, rainStyle];
+enum EffectTypes {
+  Snow = 0,
+  Rain = 1,
 }
 
 /** A React component that renders the UI specific for this sample */
@@ -27,23 +47,25 @@ export default class FireDecorationUI extends React.Component<{ iModelName: stri
   constructor(props?: any) {
     super(props);
     this.state = {
-
+      effect: EffectTypes.Snow,
     };
   }
 
   // Update the state of the sample react component by querying the API.
   private updateState() {
-    if (undefined === this.state.vp)
-      return;
+    const vp = IModelApp.viewManager.selectedView;
 
+    this.setState({ screenRectangle: vp?.getClientRect() });
   }
 
   public getControls(): React.ReactNode {
+    const entries = Object.assign({}, Object.keys(EffectTypes).filter((key: any) => isNaN(key)));
     return (<>
       <Button onClick={() => {
         const vp = IModelApp.viewManager.selectedView;
         if (undefined === vp)
           return;
+
         FireDecorator.enable();
       }}>Fire</Button>
       <Button onClick={() => {
@@ -55,12 +77,20 @@ export default class FireDecorationUI extends React.Component<{ iModelName: stri
         // });
         FireDecoratorPoint.enable();
       }}>Placement</Button>
+      <Select value={this.state.effect} options={entries} onChange={(event) => this.setState({ effect: Number.parseInt(event.target.value, 10) })} />
     </>);
   }
 
   private onIModelReady = (_imodel: IModelConnection) => {
-    IModelApp.viewManager.onViewOpen.addOnce((vp: Viewport) => {
-      this.setState({ vp });
+    IModelApp.viewManager.onViewOpen.addOnce((vp: ScreenViewport) => {
+      vp.onRender.addListener(() => {
+        const rect = vp.getClientRect();
+        const stateRect = this.state.screenRectangle;
+        if (stateRect?.width !== rect.width || stateRect?.height !== rect.height)
+          this.updateState();
+      });
+      vp.overrideDisplayStyle(styles[this.state.effect as number]);
+      this.updateState();
     });
   }
 
@@ -71,13 +101,30 @@ export default class FireDecorationUI extends React.Component<{ iModelName: stri
     return viewState;
   }
 
+  public componentDidUpdate(_prevProps: any, preState: FireDecorationState) {
+    const vp = IModelApp.viewManager.selectedView;
+    if (!vp) return;
+    if (this.state.effect !== preState.effect)
+      vp.overrideDisplayStyle(styles[this.state.effect as number]);
+  }
+
   /** The sample's render method */
   public render() {
+    const rect = this.state.screenRectangle;
 
     return (
       <>
+        <style> {"#particles span { \
+            pointer-events: none !important; \
+          }"}
+        </style>
         <ControlPane instructions="click button to start fire." controls={this.getControls()} iModelSelector={this.props.iModelSelector}></ControlPane>
         <ReloadableViewport iModelName={this.props.iModelName} onIModelReady={this.onIModelReady} getCustomViewState={this.getInitialView} />
+        {rect === undefined ? <></> :
+          <div style={{ position: "fixed", top: rect.top, left: rect.left, pointerEvents: "none" }} >
+            <Particles style={{ pointerEvents: "none" }} width={rect.width} height={rect.height} id="particles" options={effects[this.state.effect]} />
+          </div>
+        }
       </>
     );
   }
