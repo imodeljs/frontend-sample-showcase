3ccdb83b88299017e91cd69547abc9db1d8170ea
Bao Ton <Bao.Ton@Bentley.com>
2021-01-28 01:47:11 -0800

    Cover all samples that open an imodel.

--- opstests/showcase-opstests.js
+++ opstests/showcase-opstests.js
@@ -1,7 +1,21 @@
 const { Builder, By, until } = require('selenium-webdriver');
 
+async function testSamples(driver, samples) {
+  for (sample in samples) {
+    // Class imodeljs-vp only appears on successful open.
+    await driver.wait(until.elementLocated(By.className("imodeljs-vp")), 40000);
 
-(async function example() {
+    const viewportImage = await driver.wait(until.elementLocated(By.xpath("//img[@src='" + samples[sample] + "-thumbnail.png']")), 30000);
+    await viewportImage.click();
+
+    // Very first sample doesn't change the URL.
+    if (sample !== '0' && samples[sample] !== "viewport-only") {
+      await driver.wait(until.urlContains(samples[sample]), 3000);
+    }
+  }
+}
+
+(async function testSampleShowcase() {
   let driver = new Builder()
     .forBrowser('chrome')
     .build();
@@ -9,22 +23,37 @@ const { Builder, By, until } = require('selenium-webdriver');
   try {
     await driver.get('https://www.itwinjs.org/sample-showcase/frontend-sample-showcase/');
 
-    const samples = [
+    const viewerSamples = [
       "viewport-only",
       "viewer-only-2d",
       "reality-data",
       "view-attributes"
     ]
 
-    for (sample in samples) {
-      const viewportImage = await driver.wait(until.elementLocated(By.xpath("//img[@src='" + samples[sample] + "-thumbnail.png']")), 30000);
-      await viewportImage.click();
+    const viewerFeatureSamples = [
+      "display-styles",
+      "classifier",
+      "emphasize-elements",
+      "heatmap-decorator",
+      "image-export",
+      "cross-probing",
+      "marker-pin",
+      "multi-viewport",
+      "property-formatting",
+      "read-settings",
+      "shadow-study",
+      "swiping-viewport",
+      "thematic-display",
+      "tooltip-customize",
+      "view-clip",
+      "volume-query",
+      "zoom-to-elements"
+    ]
 
-      // First click doesn't change the url.
-      if (sample !== '0') {
-        await driver.wait(until.urlContains(samples[sample]), 3000);
-      }
-    }
+    await testSamples(driver, viewerSamples);
+    await driver.findElement(By.xpath("//div[@title=\"Viewer Features\"]")).click();
+    await testSamples(driver, viewerFeatureSamples);
+    await driver.findElement(By.xpath("//div[@title=\"UI Components\"]")).click();
 
   } finally {
     await driver.quit();
