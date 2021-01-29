85b727fcfc52c3d27948f0e4802cee7a24ce72a9
Bao Ton <Bao.Ton@Bentley.com>
2021-01-29 09:06:53 -0800

    Add repository and copyright.

--- opstests/showcase-opstests.js
+++ opstests/showcase-opstests.js
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
 const { Builder, By, until } = require("selenium-webdriver");
 require("chromedriver");
 
