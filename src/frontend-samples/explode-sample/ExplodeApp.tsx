a676df8e03ded118641ddfcc1b06434e8c5505a4
30239292+Ellord207@users.noreply.github.com <30239292+Ellord207@users.noreply.github.com>
2021-02-01 12:53:11 -0600

    refactored based on draft PR feedback

--- src/frontend-samples/explode-sample/ExplodeApp.tsx
+++ src/frontend-samples/explode-sample/ExplodeApp.tsx
@@ -2,30 +2,37 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import * as React from "react";
+import { ByteStream, SortedArray } from "@bentley/bentleyjs-core";
+import { Point3d, Range3d, Transform, Vector3d, XYZProps } from "@bentley/geometry-core";
 import "@bentley/icons-generic-webfont/dist/bentley-icons-generic-webfont.css";
-import "common/samples-common.scss";
-import SampleApp from "common/SampleApp";
-import ExplodeUI from "./ExplodeUI";
-import { CoordSystem, DecorateContext, Decorator, EmphasizeElements, FeatureOverrideProvider, FeatureSymbology, GraphicBranch, GraphicType, ImdlReader, IModelApp, SceneContext, ScreenViewport, TileContent, TiledGraphicsProvider, TileRequest, TileTreeReference, Viewport } from "@bentley/imodeljs-frontend";
-import { Matrix3d, Point3d, Point4d, Range3d, Transform, Vector3d } from "@bentley/geometry-core";
 import { ElementGraphicsRequestProps, FeatureAppearance, IModelTileRpcInterface, Placement3d, TileVersionInfo } from "@bentley/imodeljs-common";
-import { ByteStream } from "@bentley/bentleyjs-core";
+import { CoordSystem, DecorateContext, Decorator, EmphasizeElements, FeatureOverrideProvider, FeatureSymbology, GraphicBranch, GraphicType, ImdlReader, IModelApp, SceneContext, ScreenViewport, Tile, TileContent, TiledGraphicsProvider, TileRequest, TileTreeReference, Viewport } from "@bentley/imodeljs-frontend";
+import SampleApp from "common/SampleApp";
+import "common/samples-common.scss";
+import * as React from "react";
+import ExplodeUI from "./ExplodeUI";
+
+interface ElementDataProps {
+  id: string;
+  origin: XYZProps;
+  bBoxHigh: XYZProps;
+  bBoxLow: XYZProps;
+  pitch: number;
+  roll: number;
+  yaw: number;
+}
 
 interface ElementData {
   id: string;
   origin: Point3d;
-  bBoxHigh: Point3d;
-  bBoxLow: Point3d;
   pitch: number;
   roll: number;
   yaw: number;
 
   boundingBox: Range3d;
   transformWorld: Transform;
-  transformExplode: Transform;
-  tile: TileContent;
-  isLoaded: boolean;
+  transformExplode?: Transform;
+  tile?: TileContent;
 }
 
 function* makeIdSequence() {
@@ -80,12 +87,10 @@ function computePixelSizeInMetersAtClosestPoint(vp: Viewport, center: Point3d, r
 
 /** Creates a Range3d containing all the points using the Range3d API. */
 function getRangeUnion(elements: ElementData[]): Range3d {
-  const allPoints: Point3d[] = [];
-  elements.forEach((v) => {
-    allPoints.push(v.transformWorld.multiplyPoint3d(v.bBoxHigh));
-    allPoints.push(v.transformWorld.multiplyPoint3d(v.bBoxLow));
-  });
-  return Range3d.create(...allPoints);
+  const range = Range3d.createNull();
+  for (const elem of elements)
+    range.extendRange(elem.boundingBox);
+  return range;
 }
 
 export default class ExplodeApp implements SampleApp {
@@ -123,29 +128,52 @@ export default class ExplodeApp implements SampleApp {
   }
 
   /** This is the main entry point for creating the explosion effect.  This method orchestrates the data and hands it to the provider. */
-  public static async explodeElements(vp: Viewport, elementIds: string[], explosionFactor: number, tileVersion: TileVersionInfo): Promise<void> {
+  public static async explodeElements(vp: Viewport, elementIds: string[], explodeFactor: number, tileVersion: TileVersionInfo): Promise<void> {
     const provider = ExplodeApp.getOrCreateProvider(vp);
-    const tileLoadedCallback = () => { provider.invalidate(); vp.synchWithView(); };
-    let elements: ElementData[];
-    // TODO: actually test the ids that they are all the same
-    if (provider.data.map((e) => e.id).length === elementIds.length) {
-      // Element information is already loaded.  No need to query it again.
-      elements = provider.data;
-      provider.invalidate();
-    } else {
-      // New object, need to query for the elements
-      provider.data = [];
-      elements = await ExplodeApp.queryElements(vp, elementIds);
-      provider.data = elements;
+    const newIds: string[] = [];
+    const oldData = provider.data;
+    let data: ElementData[] = [];
+    for (const id of elementIds) {
+      let element: ElementData | undefined;
+      if (element = oldData.get(id))
+        // Tiles are already loaded.  Still need to update the explode transform.
+        data.push(element);
+      else
+        // No tiles loaded.  Need to request new ones from the backend before creating a new transform.
+        newIds.push(id);
     }
-    ExplodeApp.populateExplosionTransform(elements, explosionFactor);
+    if (newIds.length > 0) {
+      const props = await this.queryElements(vp, newIds);
+      const newData: ElementData[] = ExplodeApp.createElementData(props);
+
+      ExplodeApp.populateExplosionTransform(newData, explodeFactor);
+      for (const element of newData)
+        ExplodeApp.populateTileContent(vp, element, tileVersion).then(() => {
+          provider.data.set(element.id, element);
+      provider.invalidate();
+        });
+
+      data = data.concat(newData);
+    }
     // TODO: validate or remove
-    DebuggerDecorator.setDebugDecorator(elements, vp);
-    ExplodeApp.populateTileContent(vp, elements, tileVersion, tileLoadedCallback);
+    DebuggerDecorator.setDebugDecorator(data, vp);
+    ExplodeApp.populateExplosionTransform(data, explodeFactor);
+    provider.invalidate();
   }
 
-  private static async populateTileContent(vp: Viewport, elements: ElementData[], tileVersion: TileVersionInfo, tileLoadedCallback?: (data: ElementData) => void) {
-    elements.forEach((element) => {
+  private static createElementData(props: ElementDataProps[]): ElementData[] {
+    return props.map((element) => {
+      // Populate information from first pass
+      const placement = Placement3d.fromJSON({ origin: element.origin, angles: { pitch: element.pitch, roll: element.roll, yaw: element.yaw } });
+      const transform = placement.transform;
+      const box = Range3d.create(Point3d.fromJSON(element.bBoxLow), Point3d.fromJSON(element.bBoxHigh));
+
+      const boundingBox = transform.multiplyRange(box);
+      return { ...element, transformWorld: transform, boundingBox, origin: Point3d.fromJSON(element.origin) } as ElementData;
+    });
+  }
+
+  private static async populateTileContent(vp: Viewport, element: ElementData, tileVersion: TileVersionInfo) {
       const pixelSize = getPixelSizeInMetersAtClosestPoint(vp, element);
 
       // Round down to the nearest power of ten.
@@ -157,25 +185,19 @@ export default class ExplodeApp implements SampleApp {
         elementId: element.id,
         toleranceLog10,
         formatVersion,
-        location: element.transformExplode.toJSON(),
+      // location: element.transformExplode.toJSON(),
         // contentFlags: idProvider.contentFlags,
         // omitEdges: !this.tree.hasEdges,
         clipToProjectExtents: false,
       };
-      const loadTile = async () => {
         const response: TileRequest.ResponseData | undefined = await IModelApp.tileAdmin.requestElementGraphics(vp.iModel, props);
         if (response) {
           const content = await ExplodeApp.readResponse(vp, response);
           element.tile = content;
-          element.isLoaded = true;
         }
-        return element;
-      };
-      loadTile().then(tileLoadedCallback);
-    });
   }
 
-  /** This method calculates and populates the transform for diplacing the "exploded" elements. */
+  /** This method calculates and populates the transform for displacing the "exploded" elements. */
   private static populateExplosionTransform(elements: ElementData[], explosionFactor: number) {
     // Find the center of the range containing all element
     // Have to do it after the world transform is populated.
@@ -190,22 +212,13 @@ export default class ExplodeApp implements SampleApp {
   }
 
   /** Queries the backend for the elements to explode.  It also populates the data it can interpolate with a single pass. */
-  private static async queryElements(vp: Viewport, elementsIds: string[]): Promise<ElementData[]> {
+  private static async queryElements(vp: Viewport, elementsIds: string[]): Promise<ElementDataProps[]> {
     const query = `Select ECInstanceID,Origin,Yaw,Pitch,Roll,BBoxLow,BBoxHigh FROM BisCore:GeometricElement3d WHERE ECInstanceID IN (${elementsIds.join(",")})`;
     const results = vp.iModel.query(query);
-    const data: ElementData[] = [];
+    const data: ElementDataProps[] = [];
     let row = await results.next();
     while (row.value) {
-      const element = (row.value as ElementData);
-      element.isLoaded = false;
-
-      // Populate information from first pass
-      const placement = Placement3d.fromJSON({ origin: element.origin, angles: { pitch: element.pitch, roll: element.roll, yaw: element.yaw } });
-      const transform = placement.transform;
-      const box = Range3d.create(element.bBoxHigh, element.bBoxLow);
-
-      element.transformWorld = transform;
-      element.boundingBox = transform.multiplyRange(box);
+      const element = (row.value as ElementDataProps);
 
       data.push(element);
       row = await results.next();
@@ -233,11 +246,10 @@ export default class ExplodeApp implements SampleApp {
 
 /** This provider both hides the original graphics of the element and inserts the transformed graphics. */
 class ExplodeProvider implements TiledGraphicsProvider, FeatureOverrideProvider {
-  private static _instances = new Map<number, ExplodeProvider>();
-  public data: ElementData[] = [];
+  public data = new Map<string, ElementData>();
   /** Returns a provider associated with a given viewport. If one does not exist, it will be created. */
   public static getOrCreate(vp: Viewport) {
-    let provider = ExplodeProvider._instances.get(vp.viewportId);
+    let provider = vp.findFeatureOverrideProviderOfType(ExplodeProvider);
     if (!provider)
       provider = ExplodeProvider.createAndAdd(vp);
     return provider;
@@ -245,7 +257,6 @@ class ExplodeProvider implements TiledGraphicsProvider, FeatureOverrideProvider 
   /** Creates a provider associated with a given viewport. */
   public static createAndAdd(vp: Viewport) {
     const provider = new ExplodeProvider(vp);
-    ExplodeProvider._instances.set(vp.viewportId, provider);
     provider.add(vp);
     return provider;
   }
@@ -265,7 +276,7 @@ class ExplodeProvider implements TiledGraphicsProvider, FeatureOverrideProvider 
   public invalidate() {
     this.vp.setFeatureOverrideProviderChanged();
   }
-  private constructor(public vp: Viewport) { }
+  public constructor(public vp: Viewport) { }
 
   public addFeatureOverrides(overrides: FeatureSymbology.Overrides, _vp: Viewport): void {
     const app = FeatureAppearance.fromTransparency(1);
@@ -284,23 +295,18 @@ class ExplodeProvider implements TiledGraphicsProvider, FeatureOverrideProvider 
   /** Overrides the logic for adding this provider's graphics into the scene. */
   public addToScene(output: SceneContext): void {
     const vp = output.viewport;
+
+    for (const element of this.data.values()) {
+      if (!element.tile || !element.tile.graphic || !element.transformExplode)
+        return;
     const branch = new GraphicBranch();
     const overrides = new FeatureSymbology.Overrides(vp);
     const app = FeatureAppearance.fromTransparency(0);
-    this.data
-      .filter((element) => element.isLoaded)
-      .forEach((element) => {
-        if (undefined === element.tile.graphic)
-          return;
         overrides.overrideElement(element.id, app, true);
-        // TODO: Attempt to transform without requesting new tiles.
-        // Un-rotate
-        // translate
-        // re-rotate
         branch.add(element.tile.graphic);
-      });
     branch.symbologyOverrides = overrides;
-    output.outputGraphic(IModelApp.renderSystem.createGraphicBranch(branch, Transform.createIdentity(), {}));
+      output.outputGraphic(IModelApp.renderSystem.createGraphicBranch(branch, element.transformExplode.inverse()!, {}));
+  }
   }
 }
 
