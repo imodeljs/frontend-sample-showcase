9ea039430e6dc25b091b6cd853df0d0909a931b6
linasburneika <linas.burneika@bentley.com>
2021-02-10 11:16:09 +0200

    Webpack build configuration (#145)
    * Changed webpack config
    
    * All images optimized for webview

--- config-overrides.js
+++ config-overrides.js
@@ -20,9 +20,68 @@ module.exports = function (config, env) {
     }
   });
 
+  config.optimization.minimizer[0].options.extractComments = false;
+
+  // https://github.com/Microsoft/monaco-editor-webpack-plugin/issues/43
+  config.resolve.alias = {
+    "monaco-editor": "monaco-editor-core/esm/vs/editor/editor.api.js"
+  }
+
   return Object.assign(config, override(
     addWebpackPlugin(new MonacoWebpackPlugin({
-      nodeModulesLocations: ["node_modules"]
-    })),
+      nodeModulesLocations: ["node_modules"],
+      filename: "[name].[contenthash:8].worker.js",
+      languages: ["typescript", "javascript", "css", "scss", "json"],
+      features: [
+        //"accessibilityHelp", 
+        //"anchorSelect", 
+        "bracketMatching",
+        "caretOperations",
+        "clipboard",
+        "codeAction",
+        //"codelens", 
+        //"colorPicker", 
+        "comment",
+        "contextmenu",
+        "coreCommands",
+        "cursorUndo",
+        "dnd",
+        "documentSymbols",
+        "find",
+        "folding",
+        //"fontZoom", 
+        "format",
+        "gotoError",
+        "gotoLine",
+        "gotoSymbol",
+        "hover",
+        //"iPadShowKeyboard", 
+        "inPlaceReplace",
+        "indentation",
+        "inlineHints",
+        "inspectTokens",
+        "linesOperations",
+        "linkedEditing",
+        "links",
+        "multicursor",
+        "parameterHints",
+        //"quickCommand", 
+        //"quickHelp", 
+        "quickOutline",
+        "referenceSearch",
+        "rename",
+        "smartSelect",
+        //"snippets", 
+        "suggest",
+        //"toggleHighContrast", 
+        //"toggleTabFocusMode", 
+        //"transpose", 
+        //"unusualLineTerminators", 
+        //"viewportSemanticTokens", 
+        "wordHighlighter",
+        "wordOperations",
+        "wordPartOperations"]
+    }
+    )),
   )(config, env))
 }
