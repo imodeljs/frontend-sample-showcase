6f5124bd4148dfa3cffc9ced6593afe09f082c99
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2021-02-12 08:12:10 -0500

    Lens distortion; work around bug in imodeljs-frontend.

--- src/frontend-samples/screen-space-effects-sample/Effects.ts
+++ src/frontend-samples/screen-space-effects-sample/Effects.ts
@@ -26,9 +26,15 @@ export interface VignetteConfig {
   smoothness: number;
 }
 
+export interface LensDistortionConfig {
+  strength: number;
+  cylindricalRatio: number;
+}
+
 export interface EffectsConfig {
   readonly saturation: SaturationConfig;
   readonly vignette: VignetteConfig;
+  readonly lensDistortion: LensDistortionConfig;
 }
 
 export const effectsConfig: EffectsConfig = {
@@ -39,6 +45,10 @@ export const effectsConfig: EffectsConfig = {
     size: new Float32Array([0.25, 0.25]),
     roundness: 1.0,
     smoothness: 0.5,
+  },
+  lensDistortion: {
+    strength: 0.5,
+    cylindricalRatio: 0.5,
   },
 };
 
@@ -158,6 +168,74 @@ export const effects: Effect[] = [{
       name: "u_smoothness",
       type: UniformType.Float,
       bind: (uniform) => uniform.setUniform1f(effectsConfig.vignette.smoothness),
+    });
+  },
+}, {
+  name: "Lens Distortion",
+  textureCoordFromPosition: true,
+  source: {
+    vertex: `
+      void effectMain(vec4 position) {
+        vec2 uv = textureCoordFromPosition(position);
+        float scaledHeight = strength * height;
+        float cylAspectRatio = aspectRatio * cylindricalRatio;
+        float aspectDiagSq = aspectRatio * aspectRatio + 1.0;
+        float diagSq = scaledHeight * scaledHeight * aspectDiagSq;
+        vec2 signedUV = (2.0 * uv + vec2(-1.0, -1.0));
+
+        float z = 0.5 * sqrt(diagSq + 1.0) + 0.5;
+        float ny = (z - 1.0) / (cylAspectRatio * cylAspectRatio + 1.0);
+
+        vUVDot = sqrt(ny) * vec2(cylAspectRatio, 1.0) * signedUV;
+        vUV = vec3(0.5, 0.5, 1.0) * z + vec3(-0.5, -0.5, 0.0);
+        vUV.xy += uv;
+      }`,
+    // We simply shift pixels - we don't alter their colors.
+    fragment: `
+      vec4 effectMain() {
+        return sampleSourcePixel();
+      }`,
+    // Because we're moving pixels around, we must tell the render system where the source pixel was originally located - otherwise
+    // element locate will not work correctly.
+    sampleSourcePixel: `
+      vec3 uv = dot(vUVDot, vUVDot) * vec3(-0.5, -0.5, -1.0) + vUV;
+      return TEXTURE_PROJ(u_diffuse, uv);
+    `,
+  },
+  defineEffect: (builder: ScreenSpaceEffectBuilder) => {
+    // Lens distortion is only applicable to views with the camera enabled.
+    builder.shouldApply = (context) => context.viewport.isCameraOn;
+
+    builder.addVarying("vUV", VaryingType.Vec3);
+    builder.addVarying("vUVDot", VaryingType.Vec2);
+
+    builder.addUniform({
+      name: "strength",
+      type: UniformType.Float,
+      bind: (uniform) => uniform.setUniform1f(effectsConfig.lensDistortion.strength),
+    });
+    builder.addUniform({
+      name: "cylindricalRatio",
+      type: UniformType.Float,
+      bind: (uniform) => uniform.setUniform1f(effectsConfig.lensDistortion.cylindricalRatio),
+    });
+    builder.addUniform({
+      name: "aspectRatio",
+      type: UniformType.Float,
+      bind: (uniform, context) => uniform.setUniform1f(context.viewport.viewRect.aspect),
+    });
+    builder.addUniform({
+      name: "height",
+      type: UniformType.Float,
+      bind: (uniform, context) => {
+        let height = 0;
+        if (context.viewport.view.isCameraEnabled()) {
+          const fov = context.viewport.view.camera.lens.radians;
+          const height = Math.tan(fov / 2) / context.viewport.viewRect.aspect;
+        }
+
+        uniform.setUniform1f(height);
+      },
     });
   },
 }];
