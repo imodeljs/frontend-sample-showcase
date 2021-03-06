d0ca65f67ede641cc6daa50897f430c6ea9e4402
Paul Connelly <22944042+pmconne@users.noreply.github.com>
2021-03-06 11:18:51 -0500

    Initial view.

--- src/frontend-samples/global-display-sample/GlobalDisplayUI.tsx
+++ src/frontend-samples/global-display-sample/GlobalDisplayUI.tsx
@@ -3,7 +3,7 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import { BackgroundMapType } from "@bentley/imodeljs-common";
-import { IModelApp, ScreenViewport } from "@bentley/imodeljs-frontend";
+import { IModelApp, IModelConnection, ScreenViewport, SpatialViewState } from "@bentley/imodeljs-frontend";
 import "common/samples-common.scss";
 import { ControlPane } from "common/ControlPane/ControlPane";
 import { SandboxViewport } from "common/SandboxViewport/SandboxViewport";
@@ -49,6 +49,82 @@ export default class GlobalDisplayUI extends React.Component<GlobalDisplayUIProp
       GlobalDisplayApp.travelTo(this.state.viewport, this.state.destination);
   };
 
+  // A view of Honolulu.
+  private readonly getInitialView = async (imodel: IModelConnection ) => {
+    const viewDefinitionProps = {
+      angles: {pitch: 36.514347, roll: -152.059851, yaw: -7.099313},
+      camera: {
+        eye: [-244543.511666, -6035326.371241, -4314115.010887],
+        focusDist: 3784.058586,
+        lens: 45.95389,
+      },
+      cameraOn: true,
+      categorySelectorId: "0x825",
+      classFullName: "BisCore:SpatialViewDefinition",
+      code: {scope: "0x28", spec: "0x1c", value: ""},
+      description: "",
+      displayStyleId: "0x824",
+      extents: [3208.88177, 2815.57525, 3784.058586],
+      id: "0x822",
+      isPrivate: false,
+      model: "0x28",
+      modelSelectorId: "0x823",
+      origin: [-247935.19536, -6032887.237023, -4312920.822367],
+    };
+
+    const displayStyleProps = {
+      classFullName: "BisCore:DisplayStyle3d",
+      code: {scope: "0x28", spec: "0xa", value: ""},
+      id: "0x824",
+      model: "0x28",
+      jsonProperties: {
+        styles: {
+          backgroundMap: {
+            applyTerrain: true,
+            terrainSettings: {heightOriginMode: 0}
+          },
+          environment: {
+            ground: {
+              display: false,
+            },
+            sky: {
+              display: true,
+              groundColor: 8228728,
+              nadirColor: 3880,
+              skyColor: 16764303,
+              zenithColor: 16741686
+            }
+          },
+          viewflags: {
+            backgroundMap: true,
+            grid: false,
+            renderMode: 6,
+            visEdges: true
+          }
+        },
+      },
+    };
+
+    return SpatialViewState.createFromProps({
+      viewDefinitionProps,
+      displayStyleProps,
+      categorySelectorProps: {
+        categories: [],
+        classFullName: "BisCore:CategorySelector",
+        code: {scope: "0x28", spec: "0x8", value: ""},
+        id: "0x825",
+        model: "0x28",
+      },
+      modelSelectorProps: {
+        classFullName: "BisCore:ModelSelector",
+        code: {scope: "0x28", spec: "0x11", value: ""},
+        id: "0x823",
+        model: "0x28",
+        models: [],
+      },
+    }, imodel);
+  }
+
   public componentDidUpdate(_prevProps: GlobalDisplayUIProps, prevState: GlobalDisplayUIState) {
     const viewport = this.state.viewport;
     if (!viewport)
@@ -73,16 +149,6 @@ export default class GlobalDisplayUI extends React.Component<GlobalDisplayUIProp
     }
   }
 
-  public render() {
-    const instructions = `Type in the name of a location (e.g., "Mount Everest", "Sydney Opera House", your own address, etc), then click the button to travel there.`;
-    return (
-      <>
-        <ControlPane instructions={instructions} iModelSelector={this.props.iModelSelector} controls={this.getControls()} />
-        <SandboxViewport onIModelReady={this.onIModelReady} iModelName={this.props.iModelName} />
-      </>
-    );
-  }
-
   private getControls(): React.ReactNode {
     return (
       <div className={"sample-options-2col"} style={{ gridTemplateColumns: "1fr 1fr" }}>
@@ -99,6 +165,16 @@ export default class GlobalDisplayUI extends React.Component<GlobalDisplayUIProp
         <span />
         <Button disabled={0 === this.state.destination.length} onClick={this.travelToDestination} title={"Travel to the specified destination"}>Travel</Button>
       </div>
+    );
+  }
+
+  public render() {
+    const instructions = `Type in the name of a location (e.g., "Mount Everest", "Sydney Opera House", your own address, etc), then click the button to travel there.`;
+    return (
+      <>
+        <ControlPane instructions={instructions} iModelSelector={this.props.iModelSelector} controls={this.getControls()} />
+        <SandboxViewport onIModelReady={this.onIModelReady} iModelName={this.props.iModelName} getCustomViewState={this.getInitialView} />
+      </>
     );
   }
 }
