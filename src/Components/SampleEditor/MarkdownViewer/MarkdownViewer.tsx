f7ff0c96a58a63b4d5fbc054aa1e790dc9c94935
Stefan Retief <stefan.retief@me.com>
2021-03-11 07:20:09 -0800

    updated monaco-editor to latest (#158)
    * updated monaco-editor to latest
    
    * copyright
    
    * fixed panes not popping out after snapping closed
    
    * fixes for when screen resizes
    
    * overflow: hidden to removing scrollbar
    
    * moved overflow property

---
+++ src/Components/SampleEditor/MarkdownViewer/MarkdownViewer.tsx
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import * as React from "react";
+import Markdown from "markdown-to-jsx";
+import { Link } from "../Link/Link";
+
+export interface MarkdownViewerProps {
+  readme: string;
+  onFileClicked: (file?: string | undefined) => void;
+  onSampleClicked: (groupName: string, sampleName: string, wantScroll: boolean) => void;
+}
+
+export default class MarkdownViewer extends React.Component<MarkdownViewerProps> {
+
+  public render() {
+    const options = {
+      overrides: {
+        a: { component: Link, props: { fileClicked: this.props.onFileClicked, sampleClicked: this.props.onSampleClicked } },
+      },
+    }
+
+    return (
+      <div className="sample-editor-readme">
+        <Markdown options={options}>{this.props.readme}</Markdown>
+      </div>
+    )
+  }
+}
+
