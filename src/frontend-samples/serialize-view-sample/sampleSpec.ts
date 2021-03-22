513af9b48b13d918311b56ceb4f58fbaeec5d79e
Stefan Retief <stefan.retief@me.com>
2021-03-22 09:22:13 -0700

    changed import statement to import file directly

--- src/frontend-samples/serialize-view-sample/sampleSpec.ts
+++ src/frontend-samples/serialize-view-sample/sampleSpec.ts
@@ -2,7 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import { SampleIModels } from "@itwinjs-sandbox";
+import { SampleIModels } from "@itwinjs-sandbox/SampleIModels";
 import { SampleSpec } from "SampleSpec";
 
 export function getSerializeViewSpec(): SampleSpec {
