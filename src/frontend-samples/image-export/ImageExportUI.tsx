46b1a3b3cf82efcb3ffd23541b1769595516667b
Dustin Pilkington <dustin.lebsock@bentley.com>
2021-03-23 07:41:55 -0700

    - Image Export sample updated

--- src/frontend-samples/image-export/ImageExportUI.tsx
+++ src/frontend-samples/image-export/ImageExportUI.tsx
@@ -2,40 +2,57 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import * as React from "react";
-import { SandboxViewport } from "common/SandboxViewport/SandboxViewport";
 import "common/samples-common.scss";
-import { ControlPane } from "common/ControlPane/ControlPane";
-import { Button, ButtonType } from "@bentley/ui-core";
+import React from "react";
+import { Viewer } from "@bentley/itwin-viewer-react";
+import { AuthorizationClient, default3DUiConfig, defaultIModelList, IModelSetup, SampleIModels, SampleWidgetUiProvider } from "@itwinjs-sandbox";
 import ImageExportApp from "./ImageExportApp";
+import { Button, ButtonType } from "@bentley/ui-core";
 
-export default class ImageExportyUI extends React.Component<{ iModelName: string, iModelSelector: React.ReactNode }, {}> {
+/** Create the widget to save the image */
+const ImageExportWidget: React.FunctionComponent = () => {
+  return (
+    <>
+      <div>
+        <Button buttonType={ButtonType.Hollow} onClick={ImageExportApp.exportImage}>Save as png</Button>
+      </div>
+    </>
+  );
+};
+
+export default class ImageExportUI extends React.Component<{}, { iModelName?: SampleIModels, contextId?: string, iModelId?: string }> {
+
+  private _changeIModel = (iModelName?: SampleIModels) => {
+    IModelSetup.getIModelInfo(iModelName)
+      .then((info) => {
+        this.setState({ iModelName, contextId: info.projectId, iModelId: info.imodelId });
+      });
+  };
 
   constructor(props: any) {
     super(props);
-    this.exportImageHandler = this.exportImageHandler.bind(this);
-  }
-
-  private exportImageHandler() {
-    ImageExportApp.exportImage();
-  }
-
-  public getControls() {
-    return (
-      <div>
-        <Button buttonType={ButtonType.Hollow} onClick={ImageExportApp.exportImage.bind(ImageExportApp)}>Save as png</Button>
-      </div>
-    );
+    this.state = {};
+    this._changeIModel();
   }
 
   /** The sample's render method */
   public render() {
     return (
       <>
-        { /* Display the instructions and iModelSelector for the sample on a control pane */}
-        <ControlPane instructions="Export current viewport as image" controls={this.getControls()} iModelSelector={this.props.iModelSelector}></ControlPane>
         { /* Viewport to display the iModel */}
-        <SandboxViewport iModelName={this.props.iModelName} />
+        {this.state.contextId && this.state.iModelId && <Viewer
+          contextId={this.state.contextId}
+          iModelId={this.state.iModelId}
+          authConfig={{ oidcClient: AuthorizationClient.oidcClient }}
+          defaultUiConfig={default3DUiConfig}
+          theme="dark"
+          uiProviders={[new SampleWidgetUiProvider(
+            "Export current viewport as image",
+            <ImageExportWidget />,
+            { modelList: defaultIModelList, iModelName: this.state.iModelName!, onIModelChange: this._changeIModel }
+          )]}
+        />
+        }
       </>
     );
   }
