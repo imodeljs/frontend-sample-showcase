80b52b80a31fe9de8f84b95db7168d8d71484ccd
30239292+Ellord207@users.noreply.github.com <30239292+Ellord207@users.noreply.github.com>
2021-03-24 09:29:19 -0500

    Renamed sample

--- src/frontend-samples/snow-decoration-sample/sampleSpec.ts
+++ src/frontend-samples/snow-decoration-sample/sampleSpec.ts
@@ -8,8 +8,8 @@ import { SampleSpec } from "SampleSpec";
 
 export function getParticleSnowSampleSpec(): SampleSpec {
   return ({
-    name: "snow-sample",
-    label: "Particle Effect (Snow)",
+    name: "snow-rain-sample",
+    label: "Particle Effect (Snow & Rain)",
     image: "snow-decorator-thumbnail.png",
     customModelList: [SampleIModels.Villa, SampleIModels.House, SampleIModels.MetroStation, SampleIModels.BayTown, SampleIModels.Stadium],
     readme: async () => import("!!raw-loader!./README.md"),
