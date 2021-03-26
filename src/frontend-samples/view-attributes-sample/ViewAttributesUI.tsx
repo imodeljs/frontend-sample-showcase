4d60e75f9ec1f48c6535bf1648c89668b7290ddc
Dustin Pilkington <dustin.lebsock@bentley.com>
2021-03-26 05:42:33 -0700

    - ViewAttributes sample refactored

--- src/frontend-samples/view-attributes-sample/ViewAttributesUI.tsx
+++ src/frontend-samples/view-attributes-sample/ViewAttributesUI.tsx
@@ -6,20 +6,38 @@ import { AuthorizationClient, default3DSandboxUi, IModelSetup, SampleIModels, Sa
 import React from "react";
 import { Viewer } from "@bentley/itwin-viewer-react";
 import { ViewAttributesWidget } from "./ViewAttributesWidget";
-import { IModelConnection, ViewState } from "@bentley/imodeljs-frontend";
+import { IModelApp, IModelConnection, ScreenViewport, ViewState } from "@bentley/imodeljs-frontend";
+import ViewAttributesApp, { AttrValues, ViewFlag } from "./ViewAttributesApp";
+import { RenderMode } from "@bentley/imodeljs-common";
 
 interface ViewAttributesUIState {
   iModelName?: SampleIModels;
   contextId?: string;
   iModelId?: string;
   viewState?: ViewState;
+  iModelConnection?: IModelConnection;
+  initAttributeValues: AttrValues;
 }
 
 export default class ViewAttributesUI extends React.Component<{}, ViewAttributesUIState> {
 
   constructor(props: any) {
     super(props);
-    this.state = {};
+    this.state = {
+      initAttributeValues: {
+        renderMode: RenderMode.SmoothShade,
+        acs: false,
+        backgroundMap: true,
+        backgroundTransparency: 0.01,
+        cameraOn: true,
+        grid: false,
+        hiddenEdges: false,
+        monochrome: false,
+        shadows: false,
+        skybox: true,
+        visibleEdges: false,
+      },
+    };
     this._changeIModel();
   }
 
@@ -30,19 +48,97 @@ export default class ViewAttributesUI extends React.Component<{}, ViewAttributes
       });
   };
 
+  private _oniModelReady = (iModelConnection: IModelConnection) => {
+    IModelApp.viewManager.onViewOpen.addOnce(async (_vp: ScreenViewport) => {
+      ViewAttributesApp.setAttrValues(_vp, this.state.initAttributeValues);
+    });
+
+    ViewSetup.getDefaultView(iModelConnection)
+      .then((viewState) => {
+        this.setState({ iModelConnection, viewState });
+      });
+  };
+
+  private _onChangeAttribute = (attrValues: AttrValues) => {
+    if (this.state.iModelConnection) {
+      const vp = IModelApp.viewManager.selectedView;
+      if (vp) {
+        ViewAttributesApp.setAttrValues(vp, attrValues);
+      }
+    }
+  };
+
+  private _onChangeRenderMode = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    if (this.state.iModelConnection) {
+      const vp = IModelApp.viewManager.selectedView;
+      if (vp) {
+        let renderMode: RenderMode;
+        switch (event.target.value) {
+          case "HiddenLine": { renderMode = RenderMode.HiddenLine; break; }
+          case "SmoothShade": { renderMode = RenderMode.SmoothShade; break; }
+          case "SolidFill": { renderMode = RenderMode.SolidFill; break; }
+          default:
+          case "Wireframe": { renderMode = RenderMode.Wireframe; break; }
+        }
+        ViewAttributesApp.setRenderMode(vp, renderMode);
+      }
+    }
+  };
+
+  // Handle changes to the skybox toggle.
+  private _onChangeSkyboxToggle = (checked: boolean) => {
+    if (this.state.iModelConnection) {
+      const vp = IModelApp.viewManager.selectedView;
+      if (vp) {
+        ViewAttributesApp.setSkyboxOnOff(vp, checked);
+      }
+    }
+  };
+
+  // Handle changes to the camera toggle.
+  private _onChangeCameraToggle = (checked: boolean) => {
+    if (this.state.iModelConnection) {
+      const vp = IModelApp.viewManager.selectedView;
+      if (vp) {
+        ViewAttributesApp.setCameraOnOff(vp, checked);
+      }
+    }
+  };
+
+  // Handle changes to a view flag toggle.
+  private _onChangeViewFlagToggle = (flag: ViewFlag, checked: boolean) => {
+    if (this.state.iModelConnection) {
+      const vp = IModelApp.viewManager.selectedView;
+      if (vp) {
+        ViewAttributesApp.setViewFlag(vp, flag, checked);
+      }
+    }
+  };
+
+  // Handle changes to a view flag toggle.
+  private _onTransparencySliderChange = (min: number, max: number, num: number) => {
+    if (this.state.iModelConnection) {
+      const vp = IModelApp.viewManager.selectedView;
+      if (vp) {
+        ViewAttributesApp.setBackgroundTransparency(vp, Math.abs((num / (max + 1)) - min));
+      }
+    }
+  };
+
   private _getSampleUi = (iModelName: SampleIModels) => {
     return new SampleWidgetUiProvider(
       "Use the toggle below for displaying the reality data in the model.",
-      <ViewAttributesWidget />,
+      <ViewAttributesWidget
+        attrValues={this.state.initAttributeValues}
+        onChangeAttribute={this._onChangeAttribute}
+        onChangeRenderMode={this._onChangeRenderMode}
+        onChangeSkyboxToggle={this._onChangeSkyboxToggle}
+        onChangeCameraToggle={this._onChangeCameraToggle}
+        onChangeViewFlagToggle={this._onChangeViewFlagToggle}
+        onTransparencySliderChange={this._onTransparencySliderChange}
+      />,
       { iModelName, onIModelChange: this._changeIModel }
     );
-  };
-
-  private _oniModelReady = (iModelConnection: IModelConnection) => {
-    ViewSetup.getDefaultView(iModelConnection)
-      .then((viewState) => {
-        this.setState({ viewState });
-      });
   };
 
   /** The sample's render method */
--- src/frontend-samples/view-attributes-sample/ViewAttributesWidget.tsx
+++ src/frontend-samples/view-attributes-sample/ViewAttributesWidget.tsx
@@ -6,34 +6,25 @@ import "common/samples-common.scss";
 import React, { useEffect } from "react";
 import { IModelApp } from "@bentley/imodeljs-frontend";
 import { Select, Toggle } from "@bentley/ui-core";
-import ViewAttributesApp, { AttrValues, ViewFlag } from "./ViewAttributesApp";
+import { AttrValues, ViewFlag } from "./ViewAttributesApp";
 import { useActiveIModelConnection } from "@bentley/ui-framework";
 import { RenderMode } from "@bentley/imodeljs-common";
 
-export const ViewAttributesWidget: React.FunctionComponent = () => {
-  const iModelConnection = useActiveIModelConnection();
-  const [attrValuesState, setattrValuesState] = React.useState<AttrValues>();
+export interface ViewAttributesProps {
+  attrValues: AttrValues;
+  onChangeAttribute: (attrValues: AttrValues) => void;
+  onChangeRenderMode: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  onChangeSkyboxToggle: (checked: boolean) => void;
+  onChangeCameraToggle: (checked: boolean) => void;
+  onChangeViewFlagToggle: (flag: ViewFlag, checked: boolean) => void;
+  onTransparencySliderChange: (min: number, max: number, num: number) => void;
+}
 
-  // Only runs once because of the empty array passed as dependency [], similar to componentDidMount
-  useEffect(() => {
-    if (iModelConnection) {
-      const vp = IModelApp.viewManager.selectedView;
-      if (vp) {
-        const attrValues = ViewAttributesApp.getAttrValues(vp);
-        attrValues.backgroundTransparency = 0.01;
-        setattrValuesState(attrValues);
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+export const ViewAttributesWidget: React.FunctionComponent<ViewAttributesProps> = (viewAttributesProps) => {
+  const [attrValuesState] = React.useState<AttrValues>(viewAttributesProps.attrValues);
 
   useEffect(() => {
-    if (iModelConnection) {
-      const vp = IModelApp.viewManager.selectedView;
-      if (vp && attrValuesState) {
-        ViewAttributesApp.setAttrValues(vp, attrValuesState);
-      }
-    }
+    viewAttributesProps.onChangeAttribute(attrValuesState);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [attrValuesState]);
 
@@ -47,28 +38,6 @@ export const ViewAttributesWidget: React.FunctionComponent = () => {
     );
   };
 
-  // Handle changes to the render mode.
-  const _onChangeRenderMode = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    if (undefined === iModelConnection)
-      return;
-
-    const vp = IModelApp.viewManager.selectedView;
-    if (undefined === vp)
-      return;
-
-    let renderMode: RenderMode;
-
-    switch (event.target.value) {
-      case "HiddenLine": { renderMode = RenderMode.HiddenLine; break; }
-      case "SmoothShade": { renderMode = RenderMode.SmoothShade; break; }
-      case "SolidFill": { renderMode = RenderMode.SolidFill; break; }
-      default:
-      case "Wireframe": { renderMode = RenderMode.Wireframe; break; }
-    }
-
-    ViewAttributesApp.setRenderMode(vp, renderMode);
-  };
-
   // Create the react components for the render mode row.
   const createRenderModePicker = (label: string, info: string) => {
     const options = {
@@ -77,69 +46,34 @@ export const ViewAttributesWidget: React.FunctionComponent = () => {
       SolidFill: "Solid Fill",
       Wireframe: "Wireframe",
     };
-    const element = <Select style={{ width: "fit-content" }} onChange={_onChangeRenderMode} options={options} />;
+
+    let renderMode: string;
+    switch (attrValuesState.renderMode) {
+      case RenderMode.HiddenLine: { renderMode = "HiddenLine"; break; }
+      case RenderMode.SmoothShade: { renderMode = "SmoothShade"; break; }
+      case RenderMode.SolidFill: { renderMode = "SolidFill"; break; }
+      case RenderMode.Wireframe: { renderMode = "Wireframe"; break; }
+    }
+
+    const element = <Select style={{ width: "fit-content" }} value={renderMode} onChange={viewAttributesProps.onChangeRenderMode} options={options} />;
     return createJSXElementForAttribute(label, info, element);
-  };
-
-  // Handle changes to the skybox toggle.
-  const _onChangeSkyboxToggle = (checked: boolean) => {
-    if (undefined === iModelConnection)
-      return;
-
-    const vp = IModelApp.viewManager.selectedView;
-    if (undefined === vp)
-      return;
-
-    ViewAttributesApp.setSkyboxOnOff(vp, checked);
   };
 
   // Create the react components for the skybox toggle row.
   const createSkyboxToggle = (label: string, info: string) => {
-    if (!attrValuesState)
-      return (<></>);
-
-    const element = <Toggle isOn={attrValuesState.skybox} onChange={(checked: boolean) => _onChangeSkyboxToggle(checked)} />;
+    const element = <Toggle isOn={attrValuesState.skybox} onChange={viewAttributesProps.onChangeSkyboxToggle} />;
     return createJSXElementForAttribute(label, info, element);
-  };
-
-  // Handle changes to the camera toggle.
-  const _onChangeCameraToggle = (checked: boolean) => {
-    if (undefined === iModelConnection)
-      return;
-
-    const vp = IModelApp.viewManager.selectedView;
-    if (undefined === vp)
-      return;
-
-    ViewAttributesApp.setCameraOnOff(vp, checked);
   };
 
   // Create the react components for the camera toggle row.
   const createCameraToggle = (label: string, info: string) => {
-    if (undefined === attrValuesState)
-      return (<></>);
-
-    const element = <Toggle isOn={attrValuesState.cameraOn} onChange={(checked: boolean) => _onChangeCameraToggle(checked)} />;
+    const element = <Toggle isOn={attrValuesState.cameraOn} onChange={viewAttributesProps.onChangeCameraToggle} />;
     return createJSXElementForAttribute(label, info, element);
-  };
-
-  // Handle changes to a view flag toggle.
-  const _onChangeViewFlagToggle = (flag: ViewFlag, checked: boolean) => {
-    if (iModelConnection) {
-      const vp = IModelApp.viewManager.selectedView;
-      if (undefined === vp) {
-        return;
-      }
-      ViewAttributesApp.setViewFlag(vp, flag, checked);
-    }
   };
 
   // Create the react components for a view flag row.
   const createViewFlagToggle = (flag: ViewFlag, label: string, info: string) => {
     let flagValue: boolean;
-
-    if (!attrValuesState)
-      return (<></>);
 
     switch (flag) {
       case ViewFlag.ACS: flagValue = attrValuesState.acs; break;
@@ -151,25 +85,20 @@ export const ViewAttributesWidget: React.FunctionComponent = () => {
       case ViewFlag.VisibleEdges: flagValue = attrValuesState.visibleEdges; break;
     }
 
-    const element = <Toggle isOn={flagValue} onChange={(checked: boolean) => _onChangeViewFlagToggle(flag, checked)} />;
+    const element = <Toggle isOn={flagValue} onChange={(checked: boolean) => viewAttributesProps.onChangeViewFlagToggle(flag, checked)} />;
     return createJSXElementForAttribute(label, info, element);
   };
 
   // Create the react component for the transparency slider
   const createTransparencySlider = (label: string, info: string) => {
-    if (undefined === iModelConnection)
-      return undefined;
+    let defVal = 99;
+    if (attrValuesState.backgroundTransparency)
+      defVal = (attrValuesState.backgroundTransparency * 100) - 1;
 
-    const vp = IModelApp.viewManager.selectedView;
-    if (!vp)
-      return undefined;
-
-    const element = <input type={"range"} min={0} max={99} defaultValue={99} onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-      if (vp)
-        // The calculation used here converts the whole number range 0 to 99 into a range from 1 to 0
-        // This allows the rightmost value of the slider to be opaque, while the leftmost value is completely transparent
-        ViewAttributesApp.setBackgroundTransparency(vp, Math.abs((Number(event.target.value) / 100) - 1));
-    }} />;
+    const element = <input type={"range"} min={0} max={99} defaultValue={defVal}
+      onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+        viewAttributesProps.onTransparencySliderChange(0, 99, Number(event.target.value))}
+    />;
     return createJSXElementForAttribute(label, info, element);
   };
 
