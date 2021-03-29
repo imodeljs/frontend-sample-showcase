22c72f34afe969b2cd8631cf9e214ac02d1b3b58
Stefan Retief <stefan.retief@me.com>
2021-03-29 09:56:37 -0700

    cleared frontstage state when floating widget

--- src/@itwinjs-sandbox/components/sample-widget-control/SampleWidgetContainer.tsx
+++ src/@itwinjs-sandbox/components/sample-widget-control/SampleWidgetContainer.tsx
@@ -4,16 +4,18 @@
 *--------------------------------------------------------------------------------------------*/
 
 import { IModelApp, Viewport } from "@bentley/imodeljs-frontend";
-import { UiSettings, UiSettingsResult, UiSettingsStatus } from "@bentley/ui-core";
-import { FrontstageManager, UiFramework } from "@bentley/ui-framework";
-import React, { FunctionComponent, ReactNode, useCallback, useEffect, useRef } from "react";
+import { PointProps, SizeProps } from "@bentley/ui-core";
+import { FrontstageManager, useUiSettingsContext } from "@bentley/ui-framework";
+import { Widget } from "@bentley/ui-ninezone";
+import React, { CSSProperties, FunctionComponent, ReactNode, useCallback, useEffect, useRef } from "react";
 import "./SampleWidgetContainer.scss";
 
-
-const containerStyle = {
-  display: "inline-block",
+const containerStyle: CSSProperties = {
+  display: "block",
   position: "absolute",
   visibility: "hidden",
+  height: "100vh",
+  width: "100vw",
   zIndex: -1,
 };
 
@@ -26,9 +28,7 @@ export const measureDomNode = (node: HTMLElement) => {
   container.appendChild(content);
 
   document.body.appendChild(container);
-
-  const height = content.clientHeight;
-  const width = content.clientWidth;
+  const { height, width } = content.getBoundingClientRect();
 
   container.parentNode!.removeChild(container);
   return { height, width };
@@ -43,20 +43,34 @@ export interface SampleWidgetContainerProps {
 
 export const SampleWidgetContainer: FunctionComponent<SampleWidgetContainerProps> = ({ widgetId, instructions, iModelSelector, children }) => {
   const ref = useRef<HTMLDivElement>(null);
+  const uiSettings = useUiSettingsContext();
 
-  const moveWidget = useCallback((vp: Viewport) => {
+  const floatWidget = useCallback(async (points?: PointProps, size?: SizeProps) => {
+    await uiSettings.deleteSetting("uifw-frontstageSettings", `frontstageState[${FrontstageManager.activeFrontstageDef!.id}]`);
+    FrontstageManager.activeFrontstageDef!.restoreLayout();
+    FrontstageManager.activeFrontstageDef!.floatWidget(widgetId, points, size);
+  }, [uiSettings, widgetId]);
+
+  const moveWidget = useCallback(async (vp: Viewport) => {
     if (ref.current) {
+      await floatWidget(undefined, { height: 0, width: 0 });
       const vpDims = vp.viewRect;
       const { height, width } = measureDomNode(ref.current);
       const adjustedHeight = height + 30;
-      FrontstageManager.activeFrontstageDef!.floatWidget(widgetId, { x: vpDims.width - width - 8, y: vpDims.height - 8 }, { height: adjustedHeight, width });
+      await floatWidget({ x: (vpDims.right - width) - 32, y: vpDims.bottom - adjustedHeight - 32 }, { height: adjustedHeight, width });
     }
-  }, [ref, widgetId]);
+  }, [ref, floatWidget]);
 
   useEffect(() => {
-    const sv = IModelApp.viewManager.selectedView!
-    moveWidget(sv);
-  }, [moveWidget])
+    const sv = IModelApp.viewManager.selectedView!;
+    const unsub = sv.onResized.addListener((vp) => {
+      setTimeout(async () => moveWidget(vp), 0);
+    });
+    FrontstageManager.onFrontstageReadyEvent.addOnce(() => {
+      moveWidget(sv);
+    });
+    return unsub;
+  }, [moveWidget, uiSettings]);
 
   return (
     <div ref={ref} className="sample-widget-ui">
