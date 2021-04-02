631ad461ea5d92102e2dd5e58e4f7355c23f636c
Dustin Lebsock <dustin.lebsock@bentley.com>
2021-04-02 07:00:05 -0700

    - Removed commented out code in Empahasize elements

--- src/frontend-samples/hypermodeling-sample/HyperModelingUI.tsx
+++ src/frontend-samples/hypermodeling-sample/HyperModelingUI.tsx
@@ -2,7 +2,7 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { AuthorizationClient, default3DSandboxUi, SampleIModels, SampleWidgetUiProvider, ViewSetup } from "@itwinjs-sandbox";
+import { AuthorizationClient, default3DSandboxUi, IModelSetup, SampleIModels, SampleWidgetUiProvider, ViewSetup } from "@itwinjs-sandbox";
 import React from "react";
 import { Viewer } from "@bentley/itwin-viewer-react";
 import { HyperModelingWidget } from "./HyperModelingWidget";
@@ -41,6 +41,7 @@ export default class HyperModelingUI extends React.Component<{}, HyperModelingUI
     this.state = {
       graphics2DState: true,
     };
+    IModelSetup.setIModelList();
     this._sampleWidgetUiProvider = this._getSampleUi();
     this._uiProviders = [this._sampleWidgetUiProvider];
   }
