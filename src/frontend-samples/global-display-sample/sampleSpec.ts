1d13a7a71c1cadd3d0e2c12098528c0237592b22
Will Bentley <38542573+williamkbentley@users.noreply.github.com>
2021-04-07 11:25:38 -0400

    refactored SampleSpec

--- src/frontend-samples/global-display-sample/sampleSpec.ts
+++ src/frontend-samples/global-display-sample/sampleSpec.ts
@@ -2,22 +2,21 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { SampleSpec } from "../../Components/SampleShowcase/SampleShowcase";
-import GlobalDisplayUI from "./GlobalDisplayUI";
 import { SampleIModels } from "common/IModelSelector/IModelSelector";
+import { SampleSpec } from "SampleSpec";
 
 export function getGlobalDisplaySpec(): SampleSpec {
   return ({
     name: "global-display-sample",
     label: "Global Data",
     image: "global-display-thumbnail.png",
-    customModelList: [SampleIModels.MetroStation],
-    readme: { name: "README.md", import: import("!!raw-loader!./README.md") },
-    files: [
+    readme: async () => import("!!raw-loader!./README.md"),
+    files: () => [
       { name: "GlobalDisplayUI.tsx", import: import("!!raw-loader!./GlobalDisplayUI"), entry: true },
       { name: "GlobalDisplayApp.tsx", import: import("!!raw-loader!./GlobalDisplayApp") },
     ],
-    sampleClass: GlobalDisplayUI,
+    customModelList: [SampleIModels.MetroStation],
+    type: "GlobalDisplayUI.tsx",
   });
 }
 
