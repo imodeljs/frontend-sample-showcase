d96fae71a90d777ef27490bc5bef23fad94fe477
Stefan Retief <stefan.retief@me.com>
2021-04-08 10:30:53 -0700

    updated vieweronly2d to use itwin viewer

--- src/frontend-samples/viewer-only-2d-sample/ViewerOnly2dUI.tsx
+++ src/frontend-samples/viewer-only-2d-sample/ViewerOnly2dUI.tsx
@@ -2,126 +2,43 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-
-import * as React from "react";
-import "common/samples-common.scss";
-import { IModelConnection, ViewCreator2d, ViewState } from "@bentley/imodeljs-frontend";
-import { ModelProps } from "@bentley/imodeljs-common";
-import { SandboxViewport } from "common/SandboxViewport/SandboxViewport";
+import { AuthorizationClient, default2DSandboxUi, SampleIModels, useSampleWidget } from "@itwinjs-sandbox";
+import React, { FunctionComponent, useState } from "react";
+import { Viewer } from "@bentley/itwin-viewer-react";
+import { IModelConnection } from "@bentley/imodeljs-frontend";
 import ViewerOnly2dApp from "./ViewerOnly2dApp";
-import { ViewSetup } from "api/viewSetup";
-import { ControlPane } from "common/ControlPane/ControlPane";
+import { IModelViewportControlOptions } from "@bentley/ui-framework";
+import { ViewerOnly2dWidgetProvider } from "./ViewerOnly2dWidget";
 
-// The Props and State for this sample component
-interface ViewerOnly2dProps {
-  iModelName: string;
-  iModelSelector: React.ReactNode;
-}
+const uiProviders = [new ViewerOnly2dWidgetProvider()];
 
-interface ViewerOnly2dState {
-  imodel?: IModelConnection;
-  drawings: ModelProps[];
-  sheets: ModelProps[];
-}
+const ViewportOnly2dUI: FunctionComponent = () => {
+  const sampleIModelInfo = useSampleWidget("The 2D View Selector Widget shows a list of 2D models in this iModel.", [SampleIModels.House, SampleIModels.MetroStation]);
+  const [viewportOptions, setViewportOptions] = useState<IModelViewportControlOptions>();
 
-/** A React component that renders the UI specific for this sample */
-export default class ViewerOnly2dUI extends React.Component<ViewerOnly2dProps, ViewerOnly2dState> {
-
-  constructor(props?: any) {
-    super(props);
-    this.state = { drawings: [], sheets: [] };
-  }
-
-  /** Create a UI component with all 2D models listed */
-  private _modelSelector = () => {
-    const drawings: JSX.Element[] = this.getDrawingModelList(this.state.drawings);
-    const sheets: JSX.Element[] = this.getSheetModelList(this.state.sheets);
-
-    // Display drawing and sheet options in separate sections.
-    return (
-      <div style={{ marginTop: "20px" }}>
-        <span>Select Drawing or Sheet:</span>
-        <div className="select-up">
-          <select className="uicore-inputs-select 2d-model-selector" onChange={this._handleSelection}>
-            {drawings.length > 0 && (
-              <optgroup label="Drawings">{drawings}</optgroup>
-            )}
-            {sheets.length > 0 && (
-              <optgroup label="Sheets">{sheets}</optgroup>
-            )}
-          </select>
-        </div>
-      </div>
-    );
+  const _oniModelReady = async (iModelConnection: IModelConnection) => {
+    const result = await ViewerOnly2dApp.getInitial2DModel(iModelConnection);
+    const viewState = await ViewerOnly2dApp.createDefaultViewFor2dModel(iModelConnection, result);
+    setViewportOptions({ viewState });
   };
 
-  public getDrawingModelList(models: ModelProps[]) {
-    const drawingViews: JSX.Element[] = [];
-    models.forEach((model: ModelProps, index) => {
-      drawingViews.push(<option key={`${index}drawing`} value={`${index}drawing`}>{model.name}</option>);
-    });
-    return drawingViews;
-  }
+  return (
+    <>
+      { /* Viewport to display the iModel */}
+      {sampleIModelInfo?.iModelName && sampleIModelInfo?.contextId && sampleIModelInfo?.iModelId &&
+        <Viewer
+          contextId={sampleIModelInfo.contextId}
+          iModelId={sampleIModelInfo.iModelId}
+          authConfig={{ oidcClient: AuthorizationClient.oidcClient }}
+          viewportOptions={viewportOptions}
+          defaultUiConfig={default2DSandboxUi}
+          onIModelConnected={_oniModelReady}
+          uiProviders={uiProviders}
+          theme="dark"
+        />
+      }
+    </>
+  );
+};
 
-  public getSheetModelList(models: ModelProps[]) {
-    const sheetViews: JSX.Element[] = [];
-    models.forEach((model: ModelProps, index) => {
-      sheetViews.push(<option key={`${index}sheet`} value={`${index}sheet`}>{model.name}</option>);
-    });
-    return sheetViews;
-  }
-
-  /** When a model is selected in above list, get its view and switch to it.  */
-  private _handleSelection = async (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const index = Number.parseInt(event.target.selectedOptions[0].value, 10);
-    const modelList = event.target.selectedOptions[0].value.includes("sheet") ? this.state.sheets : this.state.drawings;
-    if (this.state.imodel) {
-      await ViewerOnly2dApp.changeViewportView(this.state.imodel, modelList[index]);
-    }
-  };
-
-  /** Components for rendering the sample's instructions and controls */
-  public getControls() {
-    return (
-      <>
-        { /* This is the ui specific for this sample.*/}
-        {this._modelSelector()}
-      </>
-    );
-  }
-
-  public getInitialView = async (imodel: IModelConnection): Promise<ViewState> => {
-    this.setState({ imodel });
-    let viewState = await ViewSetup.getDefaultView(imodel);
-    const drawingsAndSheets = await ViewerOnly2dApp.get2DModels(imodel);
-    const drawings = drawingsAndSheets.drawings;
-    const sheets = drawingsAndSheets.sheets;
-
-    const firstModel = drawings.length > 0 ? drawings[0] : sheets[0];
-    this.setState({ drawings, sheets });
-    const viewCreator = new ViewCreator2d(imodel);
-    const targetView = await viewCreator.createViewForModel(firstModel.id!, firstModel.classFullName, { vpAspect: ViewSetup.getAspectRatio() });
-
-    if (targetView) {
-      viewState = targetView;
-    }
-
-    await ViewerOnly2dApp.changeViewportView(imodel, firstModel);
-
-    const modelSelector2d = document.getElementsByClassName("2d-model-selector")[0] as HTMLSelectElement;
-    modelSelector2d.selectedIndex = 0;
-
-    return viewState;
-  };
-
-  /** The sample's render method */
-  public render() {
-
-    return (
-      <>
-        <ControlPane instructions="The picker below shows a list of 2D models in this iModel." controls={this.getControls()} iModelSelector={this.props.iModelSelector}></ControlPane>
-        <SandboxViewport iModelName={this.props.iModelName} getCustomViewState={this.getInitialView} />
-      </>
-    );
-  }
-}
+export default ViewportOnly2dUI;
---
+++ src/frontend-samples/viewer-only-2d-sample/ViewerOnly2dWidget.tsx
@@ -0,0 +1,108 @@
+import React, { useEffect } from "react";
+import { useActiveIModelConnection } from "@bentley/ui-framework";
+import { ModelProps } from "@bentley/imodeljs-common";
+import { AbstractWidgetProps, StagePanelLocation, StagePanelSection, UiItemsProvider, WidgetState } from "@bentley/ui-abstract";
+import ViewerOnly2dApp from "./ViewerOnly2dApp";
+
+interface TwoDState {
+  drawingElements: JSX.Element[];
+  sheetElements: JSX.Element[];
+  sheets: ModelProps[];
+  drawings: ModelProps[];
+}
+
+const ViewerOnly2dWidget: React.FunctionComponent = () => {
+  const iModelConnection = useActiveIModelConnection();
+  const [selected, setSelected] = React.useState<string>();
+  const [twoDState, setTwoDState] = React.useState<TwoDState>(
+    {
+      drawingElements: [],
+      sheetElements: [],
+      sheets: [],
+      drawings: [],
+    }
+  );
+
+  useEffect(() => {
+    if (iModelConnection) {
+      ViewerOnly2dApp.get2DModels(iModelConnection)
+        .then(async ({ sheets, drawings }) => {
+          const drawingElements = getDrawingModelList(drawings);
+          const sheetElements = getSheetModelList(sheets);
+          setTwoDState({ sheets, drawings, sheetElements, drawingElements });
+          return ViewerOnly2dApp.getInitial2DModel(iModelConnection, drawings, sheets);
+        })
+        .then((initial) => {
+          ViewerOnly2dApp.changeViewportView(iModelConnection, initial);
+        });
+    }
+  }, [iModelConnection]);
+
+  useEffect(() => {
+    if (selected) {
+      const index = Number.parseInt(selected, 10);
+      const modelList = selected.includes("sheet") ? twoDState.sheets : twoDState.drawings;
+      if (iModelConnection) {
+        ViewerOnly2dApp.changeViewportView(iModelConnection, modelList[index]);
+      }
+    }
+  }, [iModelConnection, selected, twoDState.drawings, twoDState.sheets]);
+
+  const getDrawingModelList = (models: ModelProps[]) => {
+    const drawingViews: JSX.Element[] = [];
+    models.forEach((model: ModelProps, index) => {
+      drawingViews.push(<option key={`${index}drawing`} value={`${index}drawing`}>{model.name}</option>);
+    });
+    return drawingViews;
+  };
+
+  const getSheetModelList = (models: ModelProps[]) => {
+    const sheetViews: JSX.Element[] = [];
+    models.forEach((model: ModelProps, index) => {
+      sheetViews.push(<option key={`${index}sheet`} value={`${index}sheet`}>{model.name}</option>);
+    });
+    return sheetViews;
+  };
+
+  /** When a model is selected in above list, get its view and switch to it.  */
+  const _handleSelection = async (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelected(event.target.selectedOptions[0].value);
+  };
+
+  // Display drawing and sheet options in separate sections.
+  return (
+    <div style={{ color: "white", padding: "8px" }}>
+      <span>Select Drawing or Sheet:</span>
+      <div className="select-up">
+        <select className="uicore-inputs-select 2d-model-selector" onChange={_handleSelection} value={selected}>
+          {twoDState.drawingElements.length > 0 && (
+            <optgroup label="Drawings">{twoDState.drawingElements}</optgroup>
+          )}
+          {twoDState.sheetElements.length > 0 && (
+            <optgroup label="Sheets">{twoDState.sheetElements}</optgroup>
+          )}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export class ViewerOnly2dWidgetProvider implements UiItemsProvider {
+  public readonly id: string = "ViewerOnly2dWidgetProvider";
+
+  public provideWidgets(_stageId: string, _stageUsage: string, location: StagePanelLocation, _section?: StagePanelSection): ReadonlyArray<AbstractWidgetProps> {
+    const widgets: AbstractWidgetProps[] = [];
+    if (location === StagePanelLocation.Bottom) {
+      widgets.push(
+        {
+          id: "ViewerOnly2dWidget",
+          label: "2D View Selector",
+          defaultState: WidgetState.Floating,
+          // eslint-disable-next-line react/display-name
+          getWidgetContent: () => <ViewerOnly2dWidget />,
+        }
+      );
+    }
+    return widgets;
+  }
+}
