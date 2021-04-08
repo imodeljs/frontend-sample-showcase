8a675c5531dd5019ec4a8880aa37b73a19ded7c0
Dustin Lebsock <dustin.lebsock@bentley.com>
2021-04-08 06:52:09 -0700

    - EmphasizeElements updated

--- src/frontend-samples/emphasize-elements-sample/EmphasizeElementsUI.tsx
+++ src/frontend-samples/emphasize-elements-sample/EmphasizeElementsUI.tsx
@@ -3,14 +3,8 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import * as React from "react";
-import { ISelectionProvider, Presentation, SelectionChangeEventArgs } from "@bentley/presentation-frontend";
-import { ColorDef } from "@bentley/imodeljs-common";
-import {
-  ClearEmphasizeAction, ClearHideAction, ClearIsolateAction, ClearOverrideAction,
-  EmphasizeAction, HideAction, IsolateAction, OverrideAction,
-} from "./EmphasizeElementsApp";
-import { EmphasizeElements, IModelApp, IModelConnection } from "@bentley/imodeljs-frontend";
-import { EmphasizeElementsWidget } from "./EmphasizeElementsWidget";
+import { IModelConnection } from "@bentley/imodeljs-frontend";
+import { EmphasizeElementsWidgetProvider } from "./EmphasizeElementsWidget";
 import { AuthorizationClient, default3DSandboxUi, IModelSetup, SampleIModels, SampleWidgetUiProvider, ViewSetup } from "@itwinjs-sandbox";
 import { Viewer } from "@bentley/itwin-viewer-react";
 import { UiItemsProvider } from "@bentley/ui-abstract";
@@ -23,20 +17,6 @@ interface EmphasizeElementsUIState {
   iModelId?: string;
   viewportOptions?: IModelViewportControlOptions;
   iModelConnection?: IModelConnection;
-  selectionIsEmpty: boolean;
-  emphasizeIsActive: boolean;
-  hideIsActive: boolean;
-  isolateIsActive: boolean;
-  overrideIsActive: boolean;
-  wantEmphasis: boolean;
-  colorValue: ColorDef;
-}
-
-enum ActionType {
-  Emphasize = "Emphasize",
-  Isolate = "Isolate",
-  Hide = "Hide",
-  Override = "Color",
 }
 
 /** A React component that renders the UI specific for this sample */
@@ -47,33 +27,14 @@ export default class EmphasizeElementsUI extends React.Component<{}, EmphasizeEl
   /** Creates an Sample instance */
   constructor(props?: any) {
     super(props);
-    this.state = {
-      selectionIsEmpty: true,
-      emphasizeIsActive: false,
-      hideIsActive: false,
-      isolateIsActive: false,
-      overrideIsActive: false,
-      wantEmphasis: true,
-      colorValue: ColorDef.red,
-    };
+    this.state = {};
     IModelSetup.setIModelList([SampleIModels.RetailBuilding, SampleIModels.MetroStation, SampleIModels.BayTown, SampleIModels.House, SampleIModels.Stadium]);
     this._changeIModel();
     this._sampleWidgetUiProvider = new SampleWidgetUiProvider(
       "Use the toggle below for displaying the reality data in the model.",
-      <EmphasizeElementsWidget
-        selectionIsEmpty={this.state.selectionIsEmpty}
-        emphasizeIsActive={this.state.emphasizeIsActive}
-        hideIsActive={this.state.hideIsActive}
-        isolateIsActive={this.state.isolateIsActive}
-        overrideIsActive={this.state.overrideIsActive}
-        wantEmphasis={this.state.wantEmphasis}
-        colorValue={this.state.colorValue}
-        onColorPick={this._onColorPick}
-        handleActionButton={this._handleActionButton}
-        handleClearButton={this._handleClearButton}
-      />
+      undefined,
     );
-    this._uiProviders = [this._sampleWidgetUiProvider];
+    this._uiProviders = [this._sampleWidgetUiProvider, new EmphasizeElementsWidgetProvider()];
   }
 
   private _changeIModel = (iModelName?: SampleIModels) => {
@@ -83,97 +44,7 @@ export default class EmphasizeElementsUI extends React.Component<{}, EmphasizeEl
       });
   };
 
-  public componentDidUpdate(_prevProps: {}, prevState: EmphasizeElementsUIState) {
-    if (prevState.selectionIsEmpty !== this.state.selectionIsEmpty)
-      this._sampleWidgetUiProvider.updateControls({ selectionIsEmpty: this.state.selectionIsEmpty });
-    if (prevState.emphasizeIsActive !== this.state.emphasizeIsActive)
-      this._sampleWidgetUiProvider.updateControls({ emphasizeIsActive: this.state.emphasizeIsActive });
-    if (prevState.hideIsActive !== this.state.hideIsActive)
-      this._sampleWidgetUiProvider.updateControls({ hideIsActive: this.state.hideIsActive });
-    if (prevState.isolateIsActive !== this.state.isolateIsActive)
-      this._sampleWidgetUiProvider.updateControls({ isolateIsActive: this.state.isolateIsActive });
-    if (prevState.overrideIsActive !== this.state.overrideIsActive)
-      this._sampleWidgetUiProvider.updateControls({ overrideIsActive: this.state.overrideIsActive });
-    if (prevState.wantEmphasis !== this.state.wantEmphasis)
-      this._sampleWidgetUiProvider.updateControls({ wantEmphasis: this.state.wantEmphasis });
-  }
-
-  public componentWillUnmount() {
-    const vp = IModelApp.viewManager.selectedView;
-
-    if (undefined === vp)
-      return;
-
-    const emph = EmphasizeElements.getOrCreate(vp);
-    emph.clearEmphasizedElements(vp);
-    emph.clearHiddenElements(vp);
-    emph.clearIsolatedElements(vp);
-    emph.clearOverriddenElements(vp);
-  }
-
-  private _onSelectionChanged = (evt: SelectionChangeEventArgs, selectionProvider: ISelectionProvider) => {
-    const selection = selectionProvider.getSelection(evt.imodel, evt.level);
-    this.setState({ selectionIsEmpty: selection.isEmpty });
-  };
-
-  private _onColorPick = (colorValue: ColorDef) => {
-    this.setState({ colorValue });
-  };
-
-  private _handleActionButton = (type: ActionType) => {
-    switch (type) {
-      default:
-      case ActionType.Emphasize: {
-        if (new EmphasizeAction(this.state.wantEmphasis).run())
-          this.setState({ emphasizeIsActive: true });
-        break;
-      }
-      case ActionType.Isolate: {
-        if (new IsolateAction().run())
-          this.setState({ isolateIsActive: true });
-        break;
-      }
-      case ActionType.Hide: {
-        if (new HideAction().run())
-          this.setState({ hideIsActive: true });
-        break;
-      }
-      case ActionType.Override: {
-        if (new OverrideAction(this.state.colorValue).run())
-          this.setState({ overrideIsActive: true });
-        break;
-      }
-    }
-  };
-
-  private _handleClearButton = (type: ActionType) => {
-    switch (type) {
-      default:
-      case ActionType.Emphasize: {
-        if (new ClearEmphasizeAction().run())
-          this.setState({ emphasizeIsActive: false });
-        break;
-      }
-      case ActionType.Isolate: {
-        if (new ClearIsolateAction().run())
-          this.setState({ isolateIsActive: false });
-        break;
-      }
-      case ActionType.Hide: {
-        if (new ClearHideAction().run())
-          this.setState({ hideIsActive: false });
-        break;
-      }
-      case ActionType.Override: {
-        if (new ClearOverrideAction().run())
-          this.setState({ overrideIsActive: false });
-        break;
-      }
-    }
-  };
-
   private _oniModelReady = async (iModelConnection: IModelConnection) => {
-    Presentation.selection.selectionChange.addListener(this._onSelectionChanged);
     ViewSetup.getDefaultView(iModelConnection)
       .then((viewState) => {
         this.setState({ viewportOptions: { viewState } });
--- src/frontend-samples/emphasize-elements-sample/EmphasizeElementsWidget.tsx
+++ src/frontend-samples/emphasize-elements-sample/EmphasizeElementsWidget.tsx
@@ -7,6 +7,10 @@ import React, { useEffect } from "react";
 import { ColorDef } from "@bentley/imodeljs-common";
 import { Button, ButtonType, Toggle } from "@bentley/ui-core";
 import { ColorPickerButton } from "@bentley/ui-components";
+import { ClearEmphasizeAction, ClearHideAction, ClearIsolateAction, ClearOverrideAction, EmphasizeAction, HideAction, IsolateAction, OverrideAction } from "./EmphasizeElementsApp";
+import { ISelectionProvider, Presentation, SelectionChangeEventArgs } from "@bentley/presentation-frontend";
+import { AbstractWidgetProps, StagePanelLocation, StagePanelSection, UiItemsProvider } from "@bentley/ui-abstract";
+import { EmphasizeElements, IModelApp } from "@bentley/imodeljs-frontend";
 
 enum ActionType {
   Emphasize = "Emphasize",
@@ -15,34 +19,97 @@ enum ActionType {
   Override = "Color",
 }
 
-export interface EmphasizeElementsProps {
-  selectionIsEmpty: boolean;
-  emphasizeIsActive: boolean;
-  hideIsActive: boolean;
-  isolateIsActive: boolean;
-  overrideIsActive: boolean;
-  wantEmphasis: boolean;
-  colorValue: ColorDef;
-  onColorPick: (colorValue: ColorDef) => void;
-  handleActionButton: (type: ActionType) => void;
-  handleClearButton: (type: ActionType) => void;
-}
+export const EmphasizeElementsWidget: React.FunctionComponent = () => {
+  const [selectionIsEmptyState, setSelectionIsEmptyState] = React.useState<boolean>(true);
+  const [emphasizeIsActiveState, setEmphasizeIsActiveState] = React.useState<boolean>(false);
+  const [hideIsActiveState, setHideIsActiveState] = React.useState<boolean>(false);
+  const [isolateIsActiveState, setIsolateIsActiveState] = React.useState<boolean>(false);
+  const [overrideIsActiveState, setOverrideIsActiveState] = React.useState<boolean>(false);
 
-export const EmphasizeElementsWidget: React.FunctionComponent<EmphasizeElementsProps> = (emphasizeElementsProps) => {
-  const [wantEmphasisState, setWantEmphasisState] = React.useState<boolean>(emphasizeElementsProps.wantEmphasis);
-  const [colorValueState, setColorValueState] = React.useState<ColorDef>(emphasizeElementsProps.colorValue);
+  const [wantEmphasisState, setWantEmphasisState] = React.useState<boolean>(true);
+  const [colorValueState, setColorValueState] = React.useState<ColorDef>(ColorDef.red);
 
   useEffect(() => {
+    Presentation.selection.selectionChange.addListener(_onSelectionChanged);
 
+    /** This function is called when the widget is destroyed, similar to ComponentWillUnmount */
+    return () => {
+      const vp = IModelApp.viewManager.selectedView;
+
+      if (undefined === vp)
+        return;
+
+      const emph = EmphasizeElements.getOrCreate(vp);
+      emph.clearEmphasizedElements(vp);
+      emph.clearHiddenElements(vp);
+      emph.clearIsolatedElements(vp);
+      emph.clearOverriddenElements(vp);
+    };
   }, []);
 
-  const _onToggleEmphasis = (wantEmphasis: boolean) => {
-    setWantEmphasisState(wantEmphasis);
+  const _onSelectionChanged = (evt: SelectionChangeEventArgs, selectionProvider: ISelectionProvider) => {
+    const selection = selectionProvider.getSelection(evt.imodel, evt.level);
+    setSelectionIsEmptyState(selection.isEmpty);
   };
 
   const _onColorPick = (colorValue: ColorDef) => {
     setColorValueState(colorValue);
-    emphasizeElementsProps.onColorPick(colorValue);
+  };
+
+  const _handleActionButton = (type: ActionType) => {
+    switch (type) {
+      default:
+      case ActionType.Emphasize: {
+        if (new EmphasizeAction(wantEmphasisState).run())
+          setEmphasizeIsActiveState(true);
+        break;
+      }
+      case ActionType.Isolate: {
+        if (new IsolateAction().run())
+          setIsolateIsActiveState(true);
+        break;
+      }
+      case ActionType.Hide: {
+        if (new HideAction().run())
+          setHideIsActiveState(true);
+        break;
+      }
+      case ActionType.Override: {
+        if (new OverrideAction(colorValueState).run())
+          setOverrideIsActiveState(true);
+        break;
+      }
+    }
+  };
+
+  const _handleClearButton = (type: ActionType) => {
+    switch (type) {
+      default:
+      case ActionType.Emphasize: {
+        if (new ClearEmphasizeAction().run())
+          setEmphasizeIsActiveState(false);
+        break;
+      }
+      case ActionType.Isolate: {
+        if (new ClearIsolateAction().run())
+          setIsolateIsActiveState(false);
+        break;
+      }
+      case ActionType.Hide: {
+        if (new ClearHideAction().run())
+          setHideIsActiveState(false);
+        break;
+      }
+      case ActionType.Override: {
+        if (new ClearOverrideAction().run())
+          setOverrideIsActiveState(false);
+        break;
+      }
+    }
+  };
+
+  const _onToggleEmphasis = (wantEmphasis: boolean) => {
+    setWantEmphasisState(wantEmphasis);
   };
 
   // Display drawing and sheet options in separate sections.
@@ -50,22 +117,42 @@ export const EmphasizeElementsWidget: React.FunctionComponent<EmphasizeElementsP
     <>
       <div className="sample-options-4col">
         <span>Emphasize</span>
-        <Toggle isOn={wantEmphasisState} showCheckmark={true} onChange={_onToggleEmphasis} disabled={emphasizeElementsProps.selectionIsEmpty} />
-        <Button buttonType={ButtonType.Primary} onClick={() => emphasizeElementsProps.handleActionButton(ActionType.Emphasize)} disabled={emphasizeElementsProps.selectionIsEmpty}>Apply</Button>
-        <Button buttonType={ButtonType.Primary} onClick={() => emphasizeElementsProps.handleClearButton(ActionType.Emphasize)} disabled={!emphasizeElementsProps.emphasizeIsActive}>Clear</Button>
+        <Toggle isOn={wantEmphasisState} showCheckmark={true} onChange={_onToggleEmphasis} disabled={selectionIsEmptyState} />
+        <Button buttonType={ButtonType.Primary} onClick={() => _handleActionButton(ActionType.Emphasize)} disabled={selectionIsEmptyState}>Apply</Button>
+        <Button buttonType={ButtonType.Primary} onClick={() => _handleClearButton(ActionType.Emphasize)} disabled={!emphasizeIsActiveState}>Clear</Button>
         <span>Hide</span>
         <span />
-        <Button buttonType={ButtonType.Primary} onClick={() => emphasizeElementsProps.handleActionButton(ActionType.Hide)} disabled={emphasizeElementsProps.selectionIsEmpty}>Apply</Button>
-        <Button buttonType={ButtonType.Primary} onClick={() => emphasizeElementsProps.handleClearButton(ActionType.Hide)} disabled={!emphasizeElementsProps.hideIsActive}>Clear</Button>
+        <Button buttonType={ButtonType.Primary} onClick={() => _handleActionButton(ActionType.Hide)} disabled={selectionIsEmptyState}>Apply</Button>
+        <Button buttonType={ButtonType.Primary} onClick={() => _handleClearButton(ActionType.Hide)} disabled={!hideIsActiveState}>Clear</Button>
         <span>Isolate</span>
         <span />
-        <Button buttonType={ButtonType.Primary} onClick={() => emphasizeElementsProps.handleActionButton(ActionType.Isolate)} disabled={emphasizeElementsProps.selectionIsEmpty}>Apply</Button>
-        <Button buttonType={ButtonType.Primary} onClick={() => emphasizeElementsProps.handleClearButton(ActionType.Isolate)} disabled={!emphasizeElementsProps.isolateIsActive}>Clear</Button>
+        <Button buttonType={ButtonType.Primary} onClick={() => _handleActionButton(ActionType.Isolate)} disabled={selectionIsEmptyState}>Apply</Button>
+        <Button buttonType={ButtonType.Primary} onClick={() => _handleClearButton(ActionType.Isolate)} disabled={!isolateIsActiveState}>Clear</Button>
         <span>Override</span>
-        <ColorPickerButton initialColor={colorValueState} onColorPick={_onColorPick} disabled={emphasizeElementsProps.selectionIsEmpty} />
-        <Button buttonType={ButtonType.Primary} onClick={() => emphasizeElementsProps.handleActionButton(ActionType.Override)} disabled={emphasizeElementsProps.selectionIsEmpty}>Apply</Button>
-        <Button buttonType={ButtonType.Primary} onClick={() => emphasizeElementsProps.handleClearButton(ActionType.Override)} disabled={!emphasizeElementsProps.overrideIsActive}>Clear</Button>
+        <ColorPickerButton initialColor={colorValueState} onColorPick={_onColorPick} disabled={selectionIsEmptyState} />
+        <Button buttonType={ButtonType.Primary} onClick={() => _handleActionButton(ActionType.Override)} disabled={selectionIsEmptyState}>Apply</Button>
+        <Button buttonType={ButtonType.Primary} onClick={() => _handleClearButton(ActionType.Override)} disabled={!overrideIsActiveState}>Clear</Button>
       </div>
     </>
   );
 };
+
+export class EmphasizeElementsWidgetProvider implements UiItemsProvider {
+  public readonly id: string = "EmphasizeElementsWidgetProvider";
+
+  public provideWidgets(_stageId: string, _stageUsage: string, location: StagePanelLocation, _section?: StagePanelSection): ReadonlyArray<AbstractWidgetProps> {
+    const widgets: AbstractWidgetProps[] = [];
+    if (location === StagePanelLocation.Bottom) {
+      widgets.push(
+        {
+          id: "EmphasizeElementsWidget",
+          label: "Emphasize Elements Selector",
+          // eslint-disable-next-line react/display-name
+          getWidgetContent: () => <EmphasizeElementsWidget />,
+        }
+      );
+    }
+    return widgets;
+  }
+
+}
