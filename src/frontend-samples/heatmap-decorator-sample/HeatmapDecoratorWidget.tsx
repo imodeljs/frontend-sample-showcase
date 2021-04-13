2d89161fc84a378c5a5969a62b965a61d127bb71
Dustin Lebsock <dustin.lebsock@bentley.com>
2021-04-13 06:08:33 -0700

    - Heatmap sample updated

---
+++ src/frontend-samples/heatmap-decorator-sample/HeatmapDecoratorWidget.tsx
@@ -0,0 +1,110 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import "common/samples-common.scss";
+import React, { useEffect } from "react";
+import { Slider, Toggle } from "@bentley/ui-core";
+import { Point3d, Range2d } from "@bentley/geometry-core";
+import { PointSelector } from "common/PointSelector/PointSelector";
+import { AbstractWidgetProps, StagePanelLocation, StagePanelSection, UiItemsProvider, WidgetState } from "@bentley/ui-abstract";
+import HeatmapDecoratorApi from "./HeatmapDecoratorApi";
+import { useActiveViewport } from "@bentley/ui-framework";
+import "./HeatmapDecorator.scss";
+
+export const HeatmapDecoratorWidget: React.FunctionComponent = () => {
+  const viewport = useActiveViewport();
+  const [showDecoratorState, setShowDecoratorState] = React.useState<boolean>(true);
+  const [spreadFactorState, setSpreadFactorState] = React.useState<number>(10);
+  const [pointsState, setPointsState] = React.useState<Point3d[]>([]);
+  const [rangeState, setRangeState] = React.useState<Range2d>(Range2d.createNull());
+
+  useEffect(() => {
+    if (viewport) {
+
+      // Grab range of the contents of the view. We'll use this to size the heatmap.
+      const range3d = viewport.view.computeFitRange();
+      const range = Range2d.createFrom(range3d);
+
+      // We'll draw the heatmap as an overlay in the center of the view's Z extents.
+      const height = range3d.high.interpolate(0.5, range3d.low).z;
+
+      HeatmapDecoratorApi.disableDecorations();
+      HeatmapDecoratorApi.decorator = undefined;
+      HeatmapDecoratorApi.setupDecorator(pointsState, range, spreadFactorState, height);
+      if (showDecoratorState) {
+        HeatmapDecoratorApi.enableDecorations();
+      }
+
+      setRangeState(range);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [viewport]);
+
+  // Effect when the points get updated
+  useEffect(() => {
+    if (HeatmapDecoratorApi.decorator)
+      HeatmapDecoratorApi.decorator.setPoints(pointsState);
+  }, [pointsState]);
+
+  useEffect(() => {
+    if (HeatmapDecoratorApi.decorator)
+      HeatmapDecoratorApi.decorator.setSpreadFactor(spreadFactorState);
+  }, [spreadFactorState]);
+
+  useEffect(() => {
+    if (showDecoratorState)
+      HeatmapDecoratorApi.enableDecorations();
+    else
+      HeatmapDecoratorApi.disableDecorations();
+  }, [showDecoratorState]);
+
+  const _onPointsChanged = (p: Point3d[]) => {
+    setPointsState(p);
+  };
+
+  const _onChangeSpreadFactor = (values: readonly number[]) => {
+    setSpreadFactorState(values[0]);
+  };
+
+  const _onChangeShowHeatmap = (checked: boolean) => {
+    setShowDecoratorState(checked);
+  };
+
+  // Display drawing and sheet options in separate sections.
+  return (
+    <>
+      <div className="sample-options">
+        <div className="sample-options-2col">
+          <span>Show Heatmap</span>
+          <Toggle isOn={showDecoratorState} onChange={_onChangeShowHeatmap} />
+          <PointSelector onPointsChanged={_onPointsChanged} range={rangeState} />
+          <span>Spread Factor</span>
+          <Slider min={0} max={100} values={[spreadFactorState]} step={1} onUpdate={_onChangeSpreadFactor} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export class HeatmapDecoratorWidgetProvider implements UiItemsProvider {
+  public readonly id: string = "HeatmapDecoratorWidgetProvider";
+
+  public provideWidgets(_stageId: string, _stageUsage: string, location: StagePanelLocation, _section?: StagePanelSection): ReadonlyArray<AbstractWidgetProps> {
+    const widgets: AbstractWidgetProps[] = [];
+    if (location === StagePanelLocation.Right) {
+      widgets.push(
+        {
+          id: "HeatmapDecoratorWidget",
+          label: "Heatmap Decorator Selector",
+          defaultState: WidgetState.Floating,
+          // eslint-disable-next-line react/display-name
+          getWidgetContent: () => <HeatmapDecoratorWidget />,
+        }
+      );
+    }
+    return widgets;
+  }
+
+}
