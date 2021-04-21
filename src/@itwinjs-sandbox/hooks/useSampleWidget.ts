25f3e14d078a8df2ad86492ddee18946e42e216d
Will Bentley <38542573+williamkbentley@users.noreply.github.com>
2021-04-21 13:03:16 -0400

    Add hilite ruleset. It gets removed on re-init of presentation

--- src/@itwinjs-sandbox/hooks/useSampleWidget.ts
+++ src/@itwinjs-sandbox/hooks/useSampleWidget.ts
@@ -4,6 +4,9 @@
 *--------------------------------------------------------------------------------------------*/
 
 import { IModelApp } from "@bentley/imodeljs-frontend";
+import { Ruleset } from "@bentley/presentation-common";
+import { Presentation } from "@bentley/presentation-frontend";
+import * as HILITE_RULESET from "@bentley/presentation-frontend/lib/presentation-frontend/selection/HiliteRules.json";
 import { UiItemsManager } from "@bentley/ui-abstract";
 import { useActiveFrontstageDef } from "@bentley/ui-framework";
 import { SampleWidgetProvider } from "@itwinjs-sandbox/components/imodel-selector/SampleWidgetProvider";
@@ -35,6 +38,21 @@ export const useSampleWidget = (instructions: string, iModelList?: SampleIModels
       setSampleIModels(iModelList);
     }
   }, [iModelList, sampleIModels]);
+
+  /**
+   * Fix to add Hilite ruleset to presentation until BUG #599922 is addressed
+   */
+  useEffect(() => {
+    const addHiliteRuleset = async () => {
+      try {
+        if (Presentation.presentation)
+          await Presentation.presentation.rulesets().add((HILITE_RULESET as any).default as Ruleset);
+      } catch {
+        // Presentation not initialized
+      }
+    };
+    addHiliteRuleset();
+  });
 
   useEffect(() => {
     const widgetProvider = new SampleWidgetProvider(instructions, sampleIModels, setIModelName);
--- src/Components/SampleVisualizer/SampleVisualizer.tsx
+++ src/Components/SampleVisualizer/SampleVisualizer.tsx
@@ -11,6 +11,8 @@ import { AuthorizationClient } from "@itwinjs-sandbox/authentication/Authorizati
 import { MovePointTool } from "common/Geometry/InteractivePointMarker";
 import { DisplayError } from "Components/ErrorBoundary/ErrorDisplay";
 import { SampleBaseApp } from "SampleBaseApp";
+import { Ruleset } from "@bentley/presentation-common";
+import * as HILITE_RULESET from "@bentley/presentation-frontend/lib/presentation-frontend/selection/HiliteRules.json";
 
 const i18nNamespace = "sample-showcase-i18n-namespace";
 const context = (require as any).context("./../../frontend-samples", true, /\.tsx$/);
@@ -79,6 +81,9 @@ export const SampleVisualizer: FunctionComponent<SampleVisualizerProps> = ({ iTw
           }
 
           setAppReady(true);
+
+          // Fix to add Hilite ruleset until Bug #599922 is addressed
+          await Presentation.presentation.rulesets().add((HILITE_RULESET as any).default as Ruleset);
         } catch (err) {
         }
       };
