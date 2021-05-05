cc4624e908b7a4f06164438e674402a38e6cad4d
Dustin Lebsock <dustin.lebsock@bentley.com>
2021-05-05 09:11:49 -0700

    - Fire Sample sets default view, fixes showing lines issue.

--- src/frontend-samples/fire-decoration-sample/FireDecorationApp.tsx
+++ src/frontend-samples/fire-decoration-sample/FireDecorationApp.tsx
@@ -2,17 +2,23 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { AuthorizationClient, default3DSandboxUi, SampleIModels, useSampleWidget } from "@itwinjs-sandbox";
+import { AuthorizationClient, default3DSandboxUi, SampleIModels, useSampleWidget, ViewSetup } from "@itwinjs-sandbox";
 import React, { FunctionComponent, useState } from "react";
 import { Viewer } from "@bentley/itwin-viewer-react";
 import { IModelViewportControlOptions } from "@bentley/ui-framework";
 import { FireDecorationWidgetProvider } from "./FireDecorationWidget";
+import { IModelConnection } from "@bentley/imodeljs-frontend";
 
 const uiProviders = [new FireDecorationWidgetProvider()];
 
 const FireDecorationApp: FunctionComponent = () => {
   const sampleIModelInfo = useSampleWidget("Use the 'Place' button to create a new fire particle emitter. After placing, use the controls to configure the new emitter.", [SampleIModels.Villa]);
-  const [viewportOptions] = useState<IModelViewportControlOptions>();
+  const [viewportOptions, setViewportOptions] = useState<IModelViewportControlOptions>();
+
+  const _oniModelReady = async (iModelConnection: IModelConnection) => {
+    const viewState = await ViewSetup.getDefaultView(iModelConnection);
+    setViewportOptions({ viewState });
+  };
 
   /** The sample's render method */
   return (
@@ -24,6 +30,7 @@ const FireDecorationApp: FunctionComponent = () => {
           iModelId={sampleIModelInfo.iModelId}
           authConfig={{ oidcClient: AuthorizationClient.oidcClient }}
           viewportOptions={viewportOptions}
+          onIModelConnected={_oniModelReady}
           defaultUiConfig={default3DSandboxUi}
           theme="dark"
           uiProviders={uiProviders}
