90676345458138c120d8a1d36f41df22d0a057c1
Dustin Lebsock <dustin.lebsock@bentley.com>
2021-05-05 09:09:00 -0700

    - Fire Emitter dropped

--- src/frontend-samples/fire-decoration-sample/FireDecorationWidget.tsx
+++ src/frontend-samples/fire-decoration-sample/FireDecorationWidget.tsx
@@ -104,8 +104,11 @@ const FireDecorationWidget: React.FunctionComponent = () => {
 
   /** Deletes the selected fire decorator emitter. */
   const dropSelected = () => {
-    selectedEmitterState?.dispose();
-    setSelectedEmitterState(undefined);
+    if (selectedEmitterState) {
+      FireDecorationApi.dropDecorator(selectedEmitterState);
+      selectedEmitterState?.dispose();
+      setSelectedEmitterState(undefined);
+    }
   };
 
   /** Deletes all decorators. */
