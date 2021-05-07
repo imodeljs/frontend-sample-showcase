3883f40a276c758e09db631569fb8869e063278f
Dustin Lebsock <dustin.lebsock@bentley.com>
2021-05-07 12:29:21 -0700

    Multi-viewport Sample iTwinViewer update (#202)
    * - Multiviewport fixed all bugs and refactored
    
    * - Fixed changing imodels causes viewport to not update bug
    
    * - remove extranneous code
    
    * - css for multiviewport sample
    
    * - MultiViewport ReadMe Update

--- src/frontend-samples/multi-viewport-sample/MultiViewportApp.tsx
+++ src/frontend-samples/multi-viewport-sample/MultiViewportApp.tsx
@@ -2,51 +2,57 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+import { AuthorizationClient, default3DSandboxUi, useSampleWidget, ViewSetup } from "@itwinjs-sandbox";
+import React, { FunctionComponent, useEffect, useState } from "react";
+import { Viewer, ViewerFrontstage } from "@bentley/itwin-viewer-react";
+import { IModelConnection } from "@bentley/imodeljs-frontend";
+import { MultiViewportWidgetProvider } from "./MultiViewportWidget";
+import { MultiViewportFrontstage } from "./MultiViewportFrontstageProvider";
+import { IModelViewportControlOptions } from "@bentley/ui-framework";
+import "./multi-view-sample.scss";
 
-import { IModelApp, ScreenViewport, SelectedViewportChangedArgs, TwoWayViewportSync, Viewport } from "@bentley/imodeljs-frontend";
-import "common/samples-common.scss";
+const uiProviders = [new MultiViewportWidgetProvider()];
+let frontStages: ViewerFrontstage[] = [];
 
-/** This class implements the interaction between the sample and the iModel.js API.  No user interface. */
-export default class MultiViewportApp {
-  public static twoWaySync: TwoWayViewportSync = new TwoWayViewportSync();
-  public static listenerCallbacks: Array<() => void> = [];
+const MultiViewportApp: FunctionComponent = () => {
+  const sampleIModelInfo = useSampleWidget("Use the controls at the top-right to navigate the model.  Toggle to sync the viewports in the controls below.  Navigating will not change the selected viewport.");
+  const [viewportOptions, setViewportOptions] = useState<IModelViewportControlOptions>();
 
-  /** Connects the views of the two provided viewports, overriding the second parameter's view with the first's view. */
-  public static connectViewports(vp1: Viewport, vp2: Viewport) {
-    MultiViewportApp.twoWaySync.connect(vp1, vp2);
-  }
-  /** Disconnects all viewports that have been synced using this instance of [TwoWayViewportSync]. */
-  public static disconnectViewports() {
-    MultiViewportApp.twoWaySync.disconnect();
-  }
+  // Cleanup frontStages
+  useEffect(() => {
+    return () => { frontStages = []; };
+  }, []);
 
-  /** Drops all active listeners. */
-  public static dispose() {
-    MultiViewportApp.listenerCallbacks.forEach((removeListener) => removeListener());
-    MultiViewportApp.listenerCallbacks.length = 0;
-  }
+  const _oniModelReady = async (iModelConnection: IModelConnection) => {
+    const viewState = await ViewSetup.getDefaultView(iModelConnection);
 
-  /** Adds a listener to IModelApp for when the selected Viewport changes.  The app will ensure the listener is removed when no longer relevant. */
-  public static listenForSelectedViewportChange(onChange: (args: SelectedViewportChangedArgs) => void) {
-    if (false === IModelApp.viewManager.onSelectedViewportChanged.has(onChange)) {
-      const removeListener = IModelApp.viewManager.onSelectedViewportChanged.addListener(onChange);
-      MultiViewportApp.listenerCallbacks.push(removeListener);
-    }
-  }
+    // Remove the last frontstage, if there was one, to reinject the initalized viewstate on modelchange
+    frontStages.pop();
+    frontStages.push({ provider: new MultiViewportFrontstage(viewState), default: true, requiresIModelConnection: true });
 
-  /** Adds a listener to IModelApp for when a View is opened.  The app will ensure the listener is removed when no longer relevant. */
-  public static listenForViewOpened(onOpen: (args: ScreenViewport) => void) {
-    if (false === IModelApp.viewManager.onViewOpen.has(onOpen)) {
-      const removeListener = IModelApp.viewManager.onViewOpen.addListener(onOpen);
-      MultiViewportApp.listenerCallbacks.push(removeListener);
-    }
-  }
+    setViewportOptions({ viewState });
+  };
 
-  /** Adds a listener to IModelApp for when a View is opened.  The app will ensure the listener is removed when no longer relevant. */
-  public static listenForViewClosed(onOpen: (args: ScreenViewport) => void) {
-    if (false === IModelApp.viewManager.onViewOpen.has(onOpen)) {
-      const removeListener = IModelApp.viewManager.onViewClose.addListener(onOpen);
-      MultiViewportApp.listenerCallbacks.push(removeListener);
-    }
-  }
-}
+  /** The sample's render method */
+  return (
+    <>
+      { /** Viewport to display the iModel */}
+      {sampleIModelInfo?.iModelName && sampleIModelInfo?.contextId && sampleIModelInfo?.iModelId &&
+        <Viewer
+          contextId={sampleIModelInfo.contextId}
+          iModelId={sampleIModelInfo.iModelId}
+          authConfig={{ oidcClient: AuthorizationClient.oidcClient }}
+          viewportOptions={viewportOptions}
+          frontstages={frontStages}
+          onIModelConnected={_oniModelReady}
+          defaultUiConfig={default3DSandboxUi}
+          theme="dark"
+          uiProviders={uiProviders}
+        />
+      }
+    </>
+  );
+
+};
+
+export default MultiViewportApp;
