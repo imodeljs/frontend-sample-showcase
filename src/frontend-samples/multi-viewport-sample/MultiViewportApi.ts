05c0c8658c8a62204581b3fa1ae5f0a63b7db25a
Dustin Lebsock <dustin.lebsock@bentley.com>
2021-05-07 07:37:56 -0700

    - Multiviewport fixed all bugs and refactored

---
+++ src/frontend-samples/multi-viewport-sample/MultiViewportApi.ts
@@ -0,0 +1,21 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+import { TwoWayViewportSync, Viewport } from "@bentley/imodeljs-frontend";
+
+/** This class implements the interaction between the sample and the iModel.js API.  No user interface. */
+export default class MultiViewportApi {
+  public static twoWaySync: TwoWayViewportSync = new TwoWayViewportSync();
+  public static listenerCallbacks: Array<() => void> = [];
+
+  /** Connects the views of the two provided viewports, overriding the second parameter's view with the first's view. */
+  public static connectViewports(vp1: Viewport, vp2: Viewport) {
+    MultiViewportApi.twoWaySync.connect(vp1, vp2);
+  }
+  /** Disconnects all viewports that have been synced using this instance of [TwoWayViewportSync]. */
+  public static disconnectViewports() {
+    MultiViewportApi.twoWaySync.disconnect();
+  }
+}
--- src/frontend-samples/multi-viewport-sample/sampleSpec.ts
+++ src/frontend-samples/multi-viewport-sample/sampleSpec.ts
@@ -10,12 +10,14 @@ export function getMultiViewportSpec(): SampleSpec {
     name: "multi-viewport-sample",
     label: "Multiple Viewports",
     image: "multi-viewport-thumbnail.png",
+    iTwinViewerReady: true,
     readme: async () => import("!!raw-loader!./README.md"),
     files: () => [
-      { name: "MultiViewportApp.tsx", import: import("!!raw-loader!./MultiViewportApp") },
-      { name: "MultiViewportUI.tsx", import: import("!!raw-loader!./MultiViewportUI"), entry: true },
+      { name: "MultiViewportApi.ts", import: import("!!raw-loader!./MultiViewportApi") },
+      { name: "MultiViewportApp.tsx", import: import("!!raw-loader!./MultiViewportApp"), entry: true },
+      { name: "MultiViewportWidget.tsx", import: import("!!raw-loader!./MultiViewportWidget") },
       { name: "multi-view-sample.scss", import: import("!!raw-loader!./multi-view-sample.scss") },
     ],
-    type: "MultiViewportUI.tsx",
+    type: "MultiViewportApp.tsx",
   });
 }
