8a2f6408634965db33629024eab669e6d7326b84
Dustin Lebsock <dustin.lebsock@bentley.com>
2021-05-10 13:26:15 -0700

    - Copyright added to ExplodeWidget

--- src/frontend-samples/explode-sample/ExplodeWidget.tsx
+++ src/frontend-samples/explode-sample/ExplodeWidget.tsx
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
 import React, { useEffect } from "react";
 import { useActiveIModelConnection, useActiveViewport } from "@bentley/ui-framework";
 import { AbstractWidgetProps, StagePanelLocation, StagePanelSection, UiItemsProvider, WidgetState } from "@bentley/ui-abstract";
