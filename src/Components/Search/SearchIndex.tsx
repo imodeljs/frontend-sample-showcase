f223ff803e9caf7d6582e2a454e39d24d8c3d4f3
Bao Ton <Bao.Ton@bentley.com>
2021-05-13 11:57:28 -0700

    Appease linting overlords and fix space regex.

--- src/Components/Search/SearchIndex.tsx
+++ src/Components/Search/SearchIndex.tsx
@@ -3,7 +3,7 @@ import { sampleManifest } from "../../sampleManifest";
 
 class SearchIndex extends Component {
 
-  sampleIndexMetadata = sampleManifest.map(group => {
+  private sampleIndexMetadata = sampleManifest.map((group) => {
     const sampleIndex: any = [];
 
     group.samples.forEach((sample) => {
@@ -11,20 +11,20 @@ class SearchIndex extends Component {
       sampleIndex.push(
         {
           contentType: "Sample",
-          objectID: group.groupName.replace(/\/s/g, "_") + "_" + sample.name,
+          objectID: `${group.groupName.replace(/\s/g, "_")}_${sample.name}`,
           groupName: group.groupName,
-          sampleName: sample.name
+          sampleName: sample.name,
         }
       );
-    })
+    });
 
     return sampleIndex;
   });
 
-  mergedSampleIndexMetadata = [].concat.apply([], this.sampleIndexMetadata);
+  private mergedSampleIndexMetadata = [].concat.apply([], this.sampleIndexMetadata);
 
-  render() {
-    return (<div id="searchIndex">{JSON.stringify(this.mergedSampleIndexMetadata)}</div>)
+  public render() {
+    return (<div id="searchIndex">{JSON.stringify(this.mergedSampleIndexMetadata)}</div>);
   }
 }
 
