8145c47d1080614f9a2f77847658242a5db39107
Bao Ton <Bao.Ton@bentley.com>
2021-05-13 16:21:21 -0700

    Working version.

--- src/Components/Search/SearchIndex.tsx
+++ src/Components/Search/SearchIndex.tsx
@@ -1,29 +1,43 @@
 import React, { useState, useEffect } from "react";
+import { SampleSpec } from "SampleSpec";
 import { sampleManifest } from "../../sampleManifest";
 
 function SearchIndex() {
   const [sampleIndex, setSampleIndex] = useState([]);
 
   useEffect(() => {
-    const sampleIndexMetadata = sampleManifest.map((group) => {
-      const sampleIndex: any = [];
 
-      group.samples.forEach((sample) => {
+    const sampleIndex: any = [];
 
-        sampleIndex.push(
-          {
-            contentType: "Sample",
-            objectID: `${group.groupName.replace(/\s/g, "_")}_${sample.name}`,
-            groupName: group.groupName,
-            sampleName: sample.name,
-          }
-        );
-      });
+    sampleManifest.map((group) => {
 
-      return sampleIndex;
+      async function asyncForEach(array: any, callback: any) {
+        for (let index = 0; index < array.length; index++) {
+          await callback(array[index], index, array);
+        }
+      }
+
+      const start = async () => {
+        await asyncForEach(group.samples, async (sample: SampleSpec) => {
+
+
+          sampleIndex.push(
+            {
+              contentType: "Sample",
+              objectID: `${group.groupName.replace(/\s/g, "_")}_${sample.name}`,
+              groupName: group.groupName,
+              sampleName: sample.name,
+              readme: sample.readme ? await sample.readme() : ""
+            }
+          );
+
+          setSampleIndex([].concat.apply([], [...sampleIndex]));
+        })
+      }
+
+      start();
     });
 
-    setSampleIndex([].concat.apply([], sampleIndexMetadata));
   }, [])
 
   return (<div id="searchIndex">{JSON.stringify(sampleIndex)}</div>);
