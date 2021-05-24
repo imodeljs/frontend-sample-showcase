3c342fc72ff108204f29faa0db8f229814883e91
Dustin Lebsock <dustin.lebsock@bentley.com>
2021-05-24 08:41:41 -0700

    - infinite spinner and comments for read settings.

--- src/frontend-samples/read-settings-sample/ReadSettingsWidget.tsx
+++ src/frontend-samples/read-settings-sample/ReadSettingsWidget.tsx
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
 import React, { ChangeEvent, useCallback, useEffect } from "react";
 import { useActiveIModelConnection } from "@bentley/ui-framework";
 import { AbstractWidgetProps, StagePanelLocation, StagePanelSection, UiItemsProvider, WidgetState } from "@bentley/ui-abstract";
@@ -29,7 +33,7 @@ const ReadSettingsWidget: React.FunctionComponent = () => {
     return _value;
   }, [settingsKeys]);
 
-  /** iModelConnection react state is very sensitive and updates a lot, so */
+  /** iModelConnection react state is very sensitive and updates constantly, so readUpdate flag is introduced to only call the API once */
   useEffect(() => {
     if (iModelConnection && settingKey && readUpdate) {
       ReadSettingsApi.readSettings(iModelConnection.iModelId!, iModelConnection.contextId!, settingKey).then((response) => {
@@ -40,7 +44,7 @@ const ReadSettingsWidget: React.FunctionComponent = () => {
     }
   }, [_parseSettingsValue, iModelConnection, readUpdate, settingKey]);
 
-  // The showcase does not have permission to write data, it is expected to fail with 403 Forbidden.
+  /** The showcase does not have permission to write data, it is expected to fail with 403 Forbidden. */
   useEffect(() => {
     if (iModelConnection && saveInProgress && settingValue && saveUpdate) {
       ReadSettingsApi.saveSettings(iModelConnection.iModelId!, iModelConnection.contextId!, settingKey, settingValue).then((response) => {
@@ -81,13 +85,12 @@ const ReadSettingsWidget: React.FunctionComponent = () => {
     );
   }, [settingResult]);
 
-  // Display drawing and sheet options in separate sections.
   return (
     <div className="sample-options">
-      {(!settingValue) && (
+      {(settingValue === undefined) && (
         <div style={{ width: "395px" }}><Spinner size={SpinnerSize.Small} /> loading...</div>
       )}
-      {(settingValue) && (<>
+      {(settingValue !== undefined) && (<>
         <div className={"sample-options-2col"} style={{ gridTemplateColumns: "1fr 1fr" }}>
           <span>Settings Name:</span>
           <Select value={settingKey} onChange={_onSettingKeySelect} style={{ width: "fit-content" }} options={settingsKeys} />
