8f21e7249cdd246ca17619a41e158162bc7e3b6f
Stefan Retief <stefan.retief@me.com>
2021-06-03 09:49:38 -0700

    updated loaders to use annotations loaders

--- src/annotation-loader.d.ts
+++ src/annotation-loader.d.ts
@@ -2,17 +2,22 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-declare module "!!raw-loader!*" {
-  const contents: string;
+export interface AnnotationStep {
+  id: number;
+  startLineNumber: number;
+  endLineNumber: number;
+  title: string;
+  file: string;
+  markdown: string;
+  skip?: boolean;
+}
+
+declare module "!annotation-loader!*" {
+  const contents: AnnotationStep[];
   export = contents;
 }
 
-declare module "!raw-loader!*" {
-  const contents: string;
-  export = contents;
-}
-
-declare module "-!raw-loader!*" {
+declare module "!annotation-raw-loader!*" {
   const contents: string;
   export = contents;
 }
