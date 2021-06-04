5c76a4588161b69dda7992fd5fe63e3807362eab
Bao Ton <Bao.Ton@bentley.com>
2021-06-04 09:06:02 -0700

    Create tags index.

--- src/Components/Search/SearchIndex.tsx
+++ src/Components/Search/SearchIndex.tsx
@@ -15,7 +15,8 @@ function SearchIndex() {
             objectID: `${group.groupName.replace(/\s/g, "_")}_${sample.name}`,
             groupName: group.groupName,
             sampleName: sample.name,
-            description: sample.description,
+            description: sample.description ? sample.description : "",
+            tags: sample.description ? sample.description!.match(/#[a-z0-9]+/gi) : []
             // readme: sample.readme ? (await sample.readme()).default : ""
           }
         );
