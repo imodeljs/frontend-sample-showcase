dd7dcd6131e2ae068c5035ccbc0d70c246405759
linasburneika <linas.burneika@bentley.com>
2021-06-11 16:17:17 +0300

    Disable gallery pane with optional query string param gallery=false (#226)
    * Disable gallery with url query param gallery=false

--- src/Components/SampleShowcase/SampleShowcase.tsx
+++ src/Components/SampleShowcase/SampleShowcase.tsx
@@ -25,11 +25,11 @@ export const SampleShowcase: FunctionComponent = () => {
   const galleryRef = React.createRef<SampleGallery>();
 
   useEffect(() => {
-    if (scrollTo && galleryRef.current) {
+    if (activeSample.galleryVisible && scrollTo && galleryRef.current) {
       galleryRef.current.scrollToActiveSample();
       setScrollTo(false);
     }
-  }, [scrollTo, galleryRef]);
+  }, [scrollTo, galleryRef, activeSample]);
 
   const onGalleryCardClicked = (groupName: string, sampleName: string, wantScroll: boolean) => {
     if (transpileResult && !window.confirm("Changes made to the code will not be saved!")) {
@@ -80,14 +80,14 @@ export const SampleShowcase: FunctionComponent = () => {
     </div>
   );
 
-  const gallery = (
+  const gallery = activeSample.galleryVisible ? (
     <SampleGallery
       group={activeSample.group}
       onChange={onGalleryCardClicked}
       ref={galleryRef}
       samples={sampleManifest}
       selected={activeSample.name} />
-  );
+  ) : undefined;
 
   return (
     <SampleShowcaseViewHandler editor={editor} visualizer={visualizer} gallery={gallery} />
