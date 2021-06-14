70d513c317142b8c01dd5d796524f8fb33fa5239
Stefan Retief <stefan.retief@me.com>
2021-06-14 08:03:48 -0700

    updated types

--- src/types/raw-loader.d.ts
+++ src/types/raw-loader.d.ts
@@ -2,18 +2,8 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import type { AnnotationStep } from "@bentley/monaco-editor";
-declare module "!walkthrough-loader!*" {
-  const contents: AnnotationStep[];
-  export = contents;
-}
 
-declare module "!editor-file-loader!*" {
-  const contents: string;
-  export = contents;
-}
-
-declare module "!!raw-loader!*" {
+declare module "raw-loader!*" {
   const contents: string;
   export = contents;
 }
@@ -24,6 +14,11 @@ declare module "!raw-loader!*" {
 }
 
 declare module "-!raw-loader!*" {
+  const contents: string;
+  export = contents;
+}
+
+declare module "!!raw-loader!*" {
   const contents: string;
   export = contents;
 }
