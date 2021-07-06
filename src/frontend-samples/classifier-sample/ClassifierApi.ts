ac503ee91534e2ab2fc23fa2dbd1873f94fe47a6
Bao Ton <Bao.Ton@bentley.com>
2021-07-06 13:40:33 -0700

    Ignore whitespace.

--- src/frontend-samples/classifier-sample/ClassifierApi.ts
+++ src/frontend-samples/classifier-sample/ClassifierApi.ts
@@ -8,96 +8,96 @@ import { ContextRealityModelState, IModelConnection, queryRealityData, ScreenVie
 import { Presentation, SelectionChangesListener } from "@bentley/presentation-frontend";
 
 export default class ClassifierApi {
-	private static _selectionListener: SelectionChangesListener;
+  private static _selectionListener: SelectionChangesListener;
 
-	public static removeSelectionListener() {
-		Presentation.selection.selectionChange.removeListener(this._selectionListener);
-	}
+  public static removeSelectionListener() {
+    Presentation.selection.selectionChange.removeListener(this._selectionListener);
+  }
 
-	public static addSelectionListener(listener: SelectionChangesListener) {
-		this._selectionListener = listener;
-		Presentation.selection.selectionChange.addListener(this._selectionListener);
-	}
+  public static addSelectionListener(listener: SelectionChangesListener) {
+    this._selectionListener = listener;
+    Presentation.selection.selectionChange.addListener(this._selectionListener);
+  }
 
-	public static async turnOnAvailableRealityModel(viewPort: ScreenViewport, imodel: IModelConnection) {
-		const style = viewPort.displayStyle.clone();
+  public static async turnOnAvailableRealityModel(viewPort: ScreenViewport, imodel: IModelConnection) {
+    const style = viewPort.displayStyle.clone();
 
-		// Get first available reality models and attach them to displayStyle
-		const availableModels: ContextRealityModelProps[] = await queryRealityData({ contextId: imodel.contextId!, filterIModel: imodel });
-		for (const crmProp of availableModels) {
-			style.attachRealityModel(crmProp);
-			viewPort.displayStyle = style;
-			break;
-		}
-	}
+    // Get first available reality models and attach them to displayStyle
+    const availableModels: ContextRealityModelProps[] = await queryRealityData({ contextId: imodel.contextId!, filterIModel: imodel });
+    for (const crmProp of availableModels) {
+      style.attachRealityModel(crmProp);
+      viewPort.displayStyle = style;
+      break;
+    }
+  }
 
-	/**
-	 * Query the iModel to get available spatial classifiers.
-	 * Also do a custom sort and filtering for the purposes of this sample.
-	 */
-	public static async getAvailableClassifierListForViewport(vp?: Viewport): Promise<{ [key: string]: string }> {
-		const models: { [key: string]: string } = {};
-		if (!vp || !(vp.view instanceof SpatialViewState))
-			return Promise.resolve(models);
+  /**
+   * Query the iModel to get available spatial classifiers.
+   * Also do a custom sort and filtering for the purposes of this sample.
+   */
+  public static async getAvailableClassifierListForViewport(vp?: Viewport): Promise<{ [key: string]: string }> {
+    const models: { [key: string]: string } = {};
+    if (!vp || !(vp.view instanceof SpatialViewState))
+      return Promise.resolve(models);
 
-		const modelQueryParams: ModelQueryParams = {
-			from: SpatialModelState.classFullName,
-			wantPrivate: false,
-		};
+    const modelQueryParams: ModelQueryParams = {
+      from: SpatialModelState.classFullName,
+      wantPrivate: false,
+    };
 
-		let curModelProps: ModelProps[] = new Array<ModelProps>();
-		curModelProps = await vp.iModel.models.queryProps(modelQueryParams);
+    let curModelProps: ModelProps[] = new Array<ModelProps>();
+    curModelProps = await vp.iModel.models.queryProps(modelQueryParams);
 
-		// Custom sort to put 'Commercial' first. It makes the best default example.
-		curModelProps = curModelProps.sort((a, b) => {
-			if (b.name?.includes("Commercial"))
-				return 1;
-			if (a.name?.includes("Commercial"))
-				return -1;
-			return a.name!.localeCompare(b.name!);
-		});
+    // Custom sort to put 'Commercial' first. It makes the best default example.
+    curModelProps = curModelProps.sort((a, b) => {
+      if (b.name?.includes("Commercial"))
+        return 1;
+      if (a.name?.includes("Commercial"))
+        return -1;
+      return a.name!.localeCompare(b.name!);
+    });
 
-		// Filter out models that are not classifiers and form {[key: string]: string } object
-		for (const modelProps of curModelProps) {
-			if (modelProps.id && modelProps.name !== "PhiladelphiaClassification" && modelProps.name !== "Philadelphia_Pictometry") {
-				const modelId = modelProps.id;
-				const name = modelProps.name ? modelProps.name : modelId;
-				models[modelId] = name.substring(0, name.indexOf(","));
-			}
-		}
+    // Filter out models that are not classifiers and form {[key: string]: string } object
+    for (const modelProps of curModelProps) {
+      if (modelProps.id && modelProps.name !== "PhiladelphiaClassification" && modelProps.name !== "Philadelphia_Pictometry") {
+        const modelId = modelProps.id;
+        const name = modelProps.name ? modelProps.name : modelId;
+        models[modelId] = name.substring(0, name.indexOf(","));
+      }
+    }
 
-		return Promise.resolve(models);
-	}
+    return Promise.resolve(models);
+  }
 
-	// Update the classifier in the ViewPort
-	public static updateRealityDataClassifiers(vp: ScreenViewport, classifier: SpatialClassifier) {
-		// Get the first reality model in the view
-		const existingRealityModels: ContextRealityModelState[] = [];
-		vp.displayStyle.forEachRealityModel(
-			(modelState: ContextRealityModelState) =>
-				existingRealityModels.push(modelState),
-		);
-		const realityModel = existingRealityModels[0];
+  // Update the classifier in the ViewPort
+  public static updateRealityDataClassifiers(vp: ScreenViewport, classifier: SpatialClassifier) {
+    // Get the first reality model in the view
+    const existingRealityModels: ContextRealityModelState[] = [];
+    vp.displayStyle.forEachRealityModel(
+      (modelState: ContextRealityModelState) =>
+        existingRealityModels.push(modelState),
+    );
+    const realityModel = existingRealityModels[0];
 
-		// Loop through all classifiers in the reality model.
-		// If the classifier exists, update it with classifier properties
-		// If the classifier is not found, add it to realityModel.classifiers
-		let existingClassifier: boolean = false;
-		if (realityModel && realityModel.classifiers) {
-			Array.from(realityModel.classifiers).forEach((storedClassifier) => {
-				if (classifier.name === storedClassifier.name) {
-					existingClassifier = true;
-					storedClassifier = SpatialClassifier.fromJSON(classifier.toJSON());
-				}
-			});
+    // Loop through all classifiers in the reality model.
+    // If the classifier exists, update it with classifier properties
+    // If the classifier is not found, add it to realityModel.classifiers
+    let existingClassifier: boolean = false;
+    if (realityModel && realityModel.classifiers) {
+      Array.from(realityModel.classifiers).forEach((storedClassifier) => {
+        if (classifier.name === storedClassifier.name) {
+          existingClassifier = true;
+          storedClassifier = SpatialClassifier.fromJSON(classifier.toJSON());
+        }
+      });
 
-			if (!existingClassifier)
-				realityModel.classifiers.add(classifier);
+      if (!existingClassifier)
+        realityModel.classifiers.add(classifier);
 
-			realityModel.classifiers.setActive(classifier);
-			vp.invalidateScene();
+      realityModel.classifiers.setActive(classifier);
+      vp.invalidateScene();
 
-			return;
-		}
-	}
+      return;
+    }
+  }
 }
--- src/frontend-samples/reality-data-sample/RealityDataApi.ts
+++ src/frontend-samples/reality-data-sample/RealityDataApi.ts
@@ -6,57 +6,57 @@
 import "common/samples-common.scss";
 import { ContextRealityModelProps, FeatureAppearance } from "@bentley/imodeljs-common";
 import {
-	ContextRealityModelState, IModelConnection, queryRealityData, ScreenViewport,
+  ContextRealityModelState, IModelConnection, queryRealityData, ScreenViewport,
 } from "@bentley/imodeljs-frontend";
 
 export default class RealityDataApi {
 
-	// START REALITY_TOGGLE_CALLBACK
-	public static async toggleRealityModel(showReality: boolean, viewPort: ScreenViewport, imodel: IModelConnection) {
-		// START DISPLAY_STYLE
-		const style = viewPort.displayStyle.clone();
+  // START REALITY_TOGGLE_CALLBACK
+  public static async toggleRealityModel(showReality: boolean, viewPort: ScreenViewport, imodel: IModelConnection) {
+    // START DISPLAY_STYLE
+    const style = viewPort.displayStyle.clone();
 
-		// Turn off the background
-		style.viewFlags.backgroundMap = false;
-		// END DISPLAY_STYLE
+    // Turn off the background
+    style.viewFlags.backgroundMap = false;
+    // END DISPLAY_STYLE
 
-		// START REALITY_MODEL_ON
-		if (showReality) {
-			// Get first available reality model and attach it to displayStyle
-			const availableModels: ContextRealityModelProps[] = await queryRealityData({ contextId: imodel.contextId!, filterIModel: imodel });
-			for (const crmProp of availableModels) {
-				style.attachRealityModel(crmProp);
-				viewPort.displayStyle = style;
-				return;
-			}
-			// END REALITY_MODEL_ON
-			// START REALITY_MODEL_OFF
-		} else {
-			// Collect reality models on displayStyle and detach
-			const models: ContextRealityModelState[] = [];
-			style.forEachRealityModel(
-				(modelState: ContextRealityModelState) => { models.push(modelState); },
-			);
-			for (const model of models)
-				style.detachRealityModelByNameAndUrl(model.name, model.url);
-			viewPort.displayStyle = style;
-		}
-		// END REALITY_MODEL_OFF
-	}
-	// END REALITY_TOGGLE_CALLBACK
+    // START REALITY_MODEL_ON
+    if (showReality) {
+      // Get first available reality model and attach it to displayStyle
+      const availableModels: ContextRealityModelProps[] = await queryRealityData({ contextId: imodel.contextId!, filterIModel: imodel });
+      for (const crmProp of availableModels) {
+        style.attachRealityModel(crmProp);
+        viewPort.displayStyle = style;
+        return;
+      }
+      // END REALITY_MODEL_ON
+      // START REALITY_MODEL_OFF
+    } else {
+      // Collect reality models on displayStyle and detach
+      const models: ContextRealityModelState[] = [];
+      style.forEachRealityModel(
+        (modelState: ContextRealityModelState) => { models.push(modelState); },
+      );
+      for (const model of models)
+        style.detachRealityModelByNameAndUrl(model.name, model.url);
+      viewPort.displayStyle = style;
+    }
+    // END REALITY_MODEL_OFF
+  }
+  // END REALITY_TOGGLE_CALLBACK
 
-	// START TRANSPARENCY
-	// Modify reality data background transparency using the Viewport API
-	public static async setRealityDataTransparency(vp: ScreenViewport, transparency: number) {
-		// For this example we want to affect the appearance of *all* reality models. Therefore, we use -1 as the index.
-		// START APPEARANCE
-		const model = vp.displayStyle.settings.contextRealityModels.models[0];
-		if (!model) return false;
-		// END APPEARANCE
-		// START OVERRIDES
-		model.appearanceOverrides = model.appearanceOverrides ? model.appearanceOverrides.clone({ transparency }) : FeatureAppearance.fromJSON({ transparency });
-		return true;
-		// END OVERRIDES
-	}
-	// END TRANSPARENCY
+  // START TRANSPARENCY
+  // Modify reality data background transparency using the Viewport API
+  public static async setRealityDataTransparency(vp: ScreenViewport, transparency: number) {
+    // For this example we want to affect the appearance of *all* reality models. Therefore, we use -1 as the index.
+    // START APPEARANCE
+    const model = vp.displayStyle.settings.contextRealityModels.models[0];
+    if (!model) return false;
+    // END APPEARANCE
+    // START OVERRIDES
+    model.appearanceOverrides = model.appearanceOverrides ? model.appearanceOverrides.clone({ transparency }) : FeatureAppearance.fromJSON({ transparency });
+    return true;
+    // END OVERRIDES
+  }
+  // END TRANSPARENCY
 }
