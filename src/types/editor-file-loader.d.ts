f5643c9a0bba98eee62adaeedbbff75afc6a9eb9
Stefan Retief <stefan.retief@me.com>
2021-07-07 11:40:42 -0700

    updated to use latest loader config

--- src/types/editor-file-loader.d.ts
+++ src/types/editor-file-loader.d.ts
@@ -3,6 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 declare module "!editor-file-loader!*" {
-  const contents: string;
+  import type { SampleSpecFile } from "SampleSpec";
+  const contents: SampleSpecFile;
   export = contents;
 }
