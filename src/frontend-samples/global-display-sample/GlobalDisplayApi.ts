5cb141a87fe3fc8d2acf4994a6bc0224e7759542
Bao Ton <Bao.Ton@bentley.com>
2021-07-07 10:37:56 -0700

    Add typing to props and a workaround for assertion error.

--- src/frontend-samples/global-display-sample/GlobalDisplayApi.ts
+++ src/frontend-samples/global-display-sample/GlobalDisplayApi.ts
@@ -2,6 +2,7 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+import { DisplayStyle3dProps, DisplayStyleProps, SpatialViewDefinitionProps, ViewDefinition3dProps, ViewDefinitionProps, ViewStateProps } from "@bentley/imodeljs-common";
 import { BingLocationProvider, IModelConnection, queryTerrainElevationOffset, ScreenViewport, SpatialViewState } from "@bentley/imodeljs-frontend";
 
 export class GlobalDisplayApi {
@@ -33,7 +34,8 @@ export class GlobalDisplayApi {
 
   // A view of Honolulu.
   public static readonly getInitialView = async (imodel: IModelConnection) => {
-    const viewDefinitionProps = {
+
+    const viewDefinitionProps: SpatialViewDefinitionProps = {
       angles: { pitch: 36.514347, roll: -152.059851, yaw: -7.099313 },
       camera: {
         eye: [-244543.511666, -6035326.371241, -4314115.010887],
@@ -54,7 +56,7 @@ export class GlobalDisplayApi {
       origin: [-247935.19536, -6032887.237023, -4312920.822367],
     };
 
-    const displayStyleProps = {
+    const displayStyleProps: DisplayStyle3dProps = {
       classFullName: "BisCore:DisplayStyle3d",
       code: { scope: "0x28", spec: "0xa", value: "" },
       id: "0x824",
