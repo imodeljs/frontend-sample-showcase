a8b02fc5995acd950f278ba0a762e6e88bcb25d4
Dustin Lebsock <dustin.lebsock@bentley.com>
2021-08-04 05:14:14 -0700

    -Beginning transformations

--- src/sampleManifest.tsx
+++ src/sampleManifest.tsx
@@ -73,6 +73,7 @@ import { getParticleSnowSampleSpec } from "frontend-samples/snow-decoration-samp
 import { getChangedElementsSpec } from "frontend-samples/changed-elements-sample/sampleSpec";
 import { getIssuesSpec } from "frontend-samples/issues-sample/sampleSpec";
 import { getValidationSpec } from "frontend-samples/validation-sample/sampleSpec";
+import { getTransformationsSpec } from "frontend-samples/transformations-sample/sampleSpec";
 
 export interface SampleSpecGroup {
   groupName: string;
@@ -169,6 +170,7 @@ export const sampleManifest: SampleSpecGroup[] = [{
     getClashReviewSpec(),
     getIssuesSpec(),
     getReadSettingsSpec(),
+    getTransformationsSpec(),
     getValidationSpec(),
   ],
 
