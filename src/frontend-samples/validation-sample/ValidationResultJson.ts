08f847ca54f3619eb8618de60d8bb6d073d7f956
tomdicarlo <24437642+tomdicarlo@users.noreply.github.com>
2021-08-23 16:24:47 -0600

    Lint

--- src/frontend-samples/validation-sample/ValidationResultJson.ts
+++ src/frontend-samples/validation-sample/ValidationResultJson.ts
@@ -1,190 +1,190 @@
 // START RESULT_DATA
 export const jsonResultData = {
-  "result": [{
-    "elementId": "0x40000000329",
-    "elementLabel": "EX-201 [4-MH] ",
-    "ruleIndex": 0,
-    "badValue": "0"
+  result: [{
+    elementId: "0x40000000329",
+    elementLabel: "EX-201 [4-MH] ",
+    ruleIndex: 0,
+    badValue: "0",
   }, {
-    "elementId": "0x40000000331",
-    "elementLabel": "EX-202 [4-MP] ",
-    "ruleIndex": 0,
-    "badValue": "0"
+    elementId: "0x40000000331",
+    elementLabel: "EX-202 [4-MP] ",
+    ruleIndex: 0,
+    badValue: "0",
   }, {
-    "elementId": "0x40000000373",
-    "elementLabel": "EX-203 [4-OJ] ",
-    "ruleIndex": 0,
-    "badValue": "0"
+    elementId: "0x40000000373",
+    elementLabel: "EX-203 [4-OJ] ",
+    ruleIndex: 0,
+    badValue: "0",
   }, {
-    "elementId": "0x4000000037b",
-    "elementLabel": "EX-204 [4-OR] ",
-    "ruleIndex": 0,
-    "badValue": "0"
+    elementId: "0x4000000037b",
+    elementLabel: "EX-204 [4-OR] ",
+    ruleIndex: 0,
+    badValue: "0",
   }, {
-    "elementId": "0x4000000038a",
-    "elementLabel": "EX-205 [4-P6] ",
-    "ruleIndex": 0,
-    "badValue": "0"
+    elementId: "0x4000000038a",
+    elementLabel: "EX-205 [4-P6] ",
+    ruleIndex: 0,
+    badValue: "0",
   }, {
-    "elementId": "0x40000000516",
-    "elementLabel": "EX-302 [4-106] ",
-    "ruleIndex": 0,
-    "badValue": "0"
+    elementId: "0x40000000516",
+    elementLabel: "EX-302 [4-106] ",
+    ruleIndex: 0,
+    badValue: "0",
   }, {
-    "elementId": "0x40000000730",
-    "elementLabel": "E-101 [4-1F4] ",
-    "ruleIndex": 0,
-    "badValue": "0"
+    elementId: "0x40000000730",
+    elementLabel: "E-101 [4-1F4] ",
+    ruleIndex: 0,
+    badValue: "0",
   }, {
-    "elementId": "0x40000000838",
-    "elementLabel": "E-102 [4-1MG] ",
-    "ruleIndex": 0,
-    "badValue": "0"
+    elementId: "0x40000000838",
+    elementLabel: "E-102 [4-1MG] ",
+    ruleIndex: 0,
+    badValue: "0",
   }, {
-    "elementId": "0x40000000a50",
-    "elementLabel": "PMP-201 [4-21C] ",
-    "ruleIndex": 0,
-    "badValue": "0"
+    elementId: "0x40000000a50",
+    elementLabel: "PMP-201 [4-21C] ",
+    ruleIndex: 0,
+    badValue: "0",
   }, {
-    "elementId": "0x40000000a9f",
-    "elementLabel": "PMP-202 [4-23J] ",
-    "ruleIndex": 0,
-    "badValue": "0"
+    elementId: "0x40000000a9f",
+    elementLabel: "PMP-202 [4-23J] ",
+    ruleIndex: 0,
+    badValue: "0",
   }, {
-    "elementId": "0x40000000dbb",
-    "elementLabel": "PMP-101 [4-2PN] ",
-    "ruleIndex": 0,
-    "badValue": "0"
+    elementId: "0x40000000dbb",
+    elementLabel: "PMP-101 [4-2PN] ",
+    ruleIndex: 0,
+    badValue: "0",
   }, {
-    "elementId": "0x40000000e0f",
-    "elementLabel": "V-101 [4-2RZ] ",
-    "ruleIndex": 0,
-    "badValue": "0"
+    elementId: "0x40000000e0f",
+    elementLabel: "V-101 [4-2RZ] ",
+    ruleIndex: 0,
+    badValue: "0",
   }, {
-    "elementId": "0x4000000050b",
-    "elementLabel": "GASKET [4-ZV] ",
-    "ruleIndex": 1,
-    "badValue": "0.2032"
+    elementId: "0x4000000050b",
+    elementLabel: "GASKET [4-ZV] ",
+    ruleIndex: 1,
+    badValue: "0.2032",
   }, {
-    "elementId": "0x40000000512",
-    "elementLabel": "GASKET [4-102] ",
-    "ruleIndex": 1,
-    "badValue": "0.2032"
+    elementId: "0x40000000512",
+    elementLabel: "GASKET [4-102] ",
+    ruleIndex: 1,
+    badValue: "0.2032",
   }, {
-    "elementId": "0x40000000bff",
-    "elementLabel": "GASKET [4-2DB] ",
-    "ruleIndex": 1,
-    "badValue": "0.2032"
+    elementId: "0x40000000bff",
+    elementLabel: "GASKET [4-2DB] ",
+    ruleIndex: 1,
+    badValue: "0.2032",
   }, {
-    "elementId": "0x40000000c68",
-    "elementLabel": "GASKET [4-2G8] ",
-    "ruleIndex": 1,
-    "badValue": "0.2032"
+    elementId: "0x40000000c68",
+    elementLabel: "GASKET [4-2G8] ",
+    ruleIndex: 1,
+    badValue: "0.2032",
   }, {
-    "elementId": "0x40000000c6e",
-    "elementLabel": "GASKET [4-2GE] ",
-    "ruleIndex": 1,
-    "badValue": "0.2032"
+    elementId: "0x40000000c6e",
+    elementLabel: "GASKET [4-2GE] ",
+    ruleIndex: 1,
+    badValue: "0.2032",
   }, {
-    "elementId": "0x40000000c7e",
-    "elementLabel": "GASKET [4-2GU] ",
-    "ruleIndex": 1,
-    "badValue": "0.2032"
+    elementId: "0x40000000c7e",
+    elementLabel: "GASKET [4-2GU] ",
+    ruleIndex: 1,
+    badValue: "0.2032",
   }, {
-    "elementId": "0x40000000c84",
-    "elementLabel": "GASKET [4-2H0] ",
-    "ruleIndex": 1,
-    "badValue": "0.2032"
+    elementId: "0x40000000c84",
+    elementLabel: "GASKET [4-2H0] ",
+    ruleIndex: 1,
+    badValue: "0.2032",
   }, {
-    "elementId": "0x40000000c94",
-    "elementLabel": "GASKET [4-2HG] ",
-    "ruleIndex": 1,
-    "badValue": "0.2032"
+    elementId: "0x40000000c94",
+    elementLabel: "GASKET [4-2HG] ",
+    ruleIndex: 1,
+    badValue: "0.2032",
   }, {
-    "elementId": "0x40000000c9a",
-    "elementLabel": "GASKET [4-2HM] ",
-    "ruleIndex": 1,
-    "badValue": "0.2032"
+    elementId: "0x40000000c9a",
+    elementLabel: "GASKET [4-2HM] ",
+    ruleIndex: 1,
+    badValue: "0.2032",
   }, {
-    "elementId": "0x40000000cf1",
-    "elementLabel": "GASKET [4-2K1] ",
-    "ruleIndex": 1,
-    "badValue": "0.2032"
+    elementId: "0x40000000cf1",
+    elementLabel: "GASKET [4-2K1] ",
+    ruleIndex: 1,
+    badValue: "0.2032",
   }, {
-    "elementId": "0x40000000cf7",
-    "elementLabel": "GASKET [4-2K7] ",
-    "ruleIndex": 1,
-    "badValue": "0.2032"
+    elementId: "0x40000000cf7",
+    elementLabel: "GASKET [4-2K7] ",
+    ruleIndex: 1,
+    badValue: "0.2032",
   }, {
-    "elementId": "0x40000000e13",
-    "elementLabel": "GASKET [4-2S3] ",
-    "ruleIndex": 1,
-    "badValue": "0.254"
+    elementId: "0x40000000e13",
+    elementLabel: "GASKET [4-2S3] ",
+    ruleIndex: 1,
+    badValue: "0.254",
   }, {
-    "elementId": "0x4000000038b",
-    "elementLabel": "EX-205-N1 [4-P7] ",
-    "ruleIndex": 2,
-    "badValue": "Undefined"
+    elementId: "0x4000000038b",
+    elementLabel: "EX-205-N1 [4-P7] ",
+    ruleIndex: 2,
+    badValue: "Undefined",
   }, {
-    "elementId": "0x40000000517",
-    "elementLabel": "EX-302-N2 [4-107] ",
-    "ruleIndex": 2,
-    "badValue": "Undefined"
+    elementId: "0x40000000517",
+    elementLabel: "EX-302-N2 [4-107] ",
+    ruleIndex: 2,
+    badValue: "Undefined",
   }, {
-    "elementId": "0x40000000524",
-    "elementLabel": "V-301-N2 [4-10K] ",
-    "ruleIndex": 2,
-    "badValue": "Undefined"
+    elementId: "0x40000000524",
+    elementLabel: "V-301-N2 [4-10K] ",
+    ruleIndex: 2,
+    badValue: "Undefined",
   }, {
-    "elementId": "0x40000000575",
-    "elementLabel": "EX-302-N3 [4-12T] ",
-    "ruleIndex": 2,
-    "badValue": "Undefined"
+    elementId: "0x40000000575",
+    elementLabel: "EX-302-N3 [4-12T] ",
+    ruleIndex: 2,
+    badValue: "Undefined",
   }, {
-    "elementId": "0x40000000733",
-    "elementLabel": "E-101-N1 [4-1F7] ",
-    "ruleIndex": 2,
-    "badValue": "Undefined"
+    elementId: "0x40000000733",
+    elementLabel: "E-101-N1 [4-1F7] ",
+    ruleIndex: 2,
+    badValue: "Undefined",
   }, {
-    "elementId": "0x40000000796",
-    "elementLabel": "E-101-N3 [4-1HY] ",
-    "ruleIndex": 2,
-    "badValue": "Undefined"
+    elementId: "0x40000000796",
+    elementLabel: "E-101-N3 [4-1HY] ",
+    ruleIndex: 2,
+    badValue: "Undefined",
   }, {
-    "elementId": "0x4000000083b",
-    "elementLabel": "E-102-N1 [4-1MJ] ",
-    "ruleIndex": 2,
-    "badValue": "Undefined"
+    elementId: "0x4000000083b",
+    elementLabel: "E-102-N1 [4-1MJ] ",
+    ruleIndex: 2,
+    badValue: "Undefined",
   }, {
-    "elementId": "0x40000000855",
-    "elementLabel": "E-102-N3 [4-1N9] ",
-    "ruleIndex": 2,
-    "badValue": "Undefined"
+    elementId: "0x40000000855",
+    elementLabel: "E-102-N3 [4-1N9] ",
+    ruleIndex: 2,
+    badValue: "Undefined",
   }, {
-    "elementId": "0x40000000d46",
-    "elementLabel": "V-301-N7 [4-2ME] ",
-    "ruleIndex": 2,
-    "badValue": "Undefined"
+    elementId: "0x40000000d46",
+    elementLabel: "V-301-N7 [4-2ME] ",
+    ruleIndex: 2,
+    badValue: "Undefined",
   }, {
-    "elementId": "0x40000000d48",
-    "elementLabel": "V-301-N8 [4-2MG] ",
-    "ruleIndex": 2,
-    "badValue": "Undefined"
+    elementId: "0x40000000d48",
+    elementLabel: "V-301-N8 [4-2MG] ",
+    ruleIndex: 2,
+    badValue: "Undefined",
   }, {
-    "elementId": "0x40000000d4a",
-    "elementLabel": "V-301-N9 [4-2MI] ",
-    "ruleIndex": 2,
-    "badValue": "Undefined"
+    elementId: "0x40000000d4a",
+    elementLabel: "V-301-N9 [4-2MI] ",
+    ruleIndex: 2,
+    badValue: "Undefined",
   }],
-  "ruleList": [{
-    "id": "1FapLmZ23ESON8qLonyWkvTrFm7XKN1KtKisuk1_eds",
-    "displayName": "NonZeroHeightCheck"
+  ruleList: [{
+    id: "UcJ9slMONkqaOBgvwwm-BxhaAWxdDAZHs6JvnySyITk",
+    displayName: "MinimumHeight",
   }, {
-    "id": "1FapLmZ23ESON8qLonyWkts_0vi1uB1GqMcVkFTRino",
-    "displayName": "NominalDiameterRange"
+    id: "UcJ9slMONkqaOBgvwwm-BwHRtttv0KJLnVI9yt0PGsQ",
+    displayName: "NominalDiameterRange",
   }, {
-    "id": "1FapLmZ23ESON8qLonyWko2adcMCqqxLsRlqU7yzo0E",
-    "displayName": "FuncInstIDSet"
-  }]
-}
+    id: "UcJ9slMONkqaOBgvwwm-B3fu6BQSba5AsWlFi2CthUs",
+    displayName: "FuncInstIdDefined",
+  }],
+};
 // END RESULT_DATA
