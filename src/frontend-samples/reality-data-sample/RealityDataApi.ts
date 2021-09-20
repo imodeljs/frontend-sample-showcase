eb1f50ed6fedb109b847238c42efc334172fae70
Will Bentley <38542573+williamkbentley@users.noreply.github.com>
2021-09-20 09:48:40 -0400

    Upgrade imjs version. WIP

--- src/frontend-samples/reality-data-sample/RealityDataApi.ts
+++ src/frontend-samples/reality-data-sample/RealityDataApi.ts
@@ -14,39 +14,6 @@ export default class RealityDataApi {
     return availableModels;
   }
 
-  // START REALITY_TOGGLE_CALLBACK
-  /* public static async toggleRealityModel(showReality: boolean, viewPort: ScreenViewport, imodel: IModelConnection) {
-    // START DISPLAY_STYLE
-    const style = viewPort.displayStyle.clone();
-
-    // Turn off the background
-    style.viewFlags.backgroundMap = false;
-    // END DISPLAY_STYLE
-
-    // START REALITY_MODEL_ON
-    if (showReality) {
-      // Get first available reality model and attach it to displayStyle
-      const availableModels: ContextRealityModelProps[] = await queryRealityData({ contextId: imodel.contextId!, filterIModel: imodel });
-      for (const crmProp of availableModels) {
-        style.attachRealityModel(crmProp);
-        viewPort.displayStyle = style;
-        return;
-      }
-      // END REALITY_MODEL_ON
-      // START REALITY_MODEL_OFF
-    } else {
-      // Collect reality models on displayStyle and detach
-      const models: ContextRealityModelState[] = [];
-      style.forEachRealityModel(
-        (modelState: ContextRealityModelState) => { models.push(modelState); },
-      );
-      for (const model of models)
-        style.detachRealityModelByNameAndUrl(model.name, model.url);
-      viewPort.displayStyle = style;
-    }
-    // END REALITY_MODEL_OFF
-  }*/
-
   public static toggleRealityModel(crmProp: ContextRealityModelProps, viewPort: ScreenViewport, show?: boolean) {
     const style = viewPort.displayStyle.clone();
 
@@ -54,7 +21,6 @@ export default class RealityDataApi {
     const crmName = crmProp.name ? crmProp.name : "";
 
     if (show && !style.hasAttachedRealityModel(crmName, crmProp.tilesetUrl)) {
-
       // Form orbitGtBlob if realityDataType is OPC
       let orbitGtBlob: OrbitGtBlobProps | undefined;
       if (crmProp.orbitGtBlob) {
@@ -66,7 +32,6 @@ export default class RealityDataApi {
           accountName: crmProp.realityDataId ? crmProp.realityDataId : "",
         };
       }
-
       const unattached: ContextRealityModelProps = {
         tilesetUrl: crmProp.tilesetUrl,
         name: crmProp.name ? crmProp.name : "Unnamed",
@@ -75,26 +40,16 @@ export default class RealityDataApi {
         orbitGtBlob,
         classifiers: [],
       };
-
-      console.log(`turning on ${crmProp.name}`);
-      console.log(`${JSON.stringify(unattached)}`);
-      style.attachRealityModel(unattached);
+      viewPort.displayStyle.attachRealityModel(unattached);
     } else if (!show) {
-      console.log(`turning off ${crmProp.name}`);
-      style.detachRealityModelByNameAndUrl(crmName, crmProp.tilesetUrl);
+      viewPort.displayStyle.detachRealityModelByNameAndUrl(crmName, crmProp.tilesetUrl);
     }
-
-    viewPort.displayStyle = style;
-
-    const clonedView = viewPort.view.clone();
-    console.log(clonedView.toProps());
+    viewPort.invalidateScene();
   }
-  // END REALITY_TOGGLE_CALLBACK
 
   // START TRANSPARENCY
   // Modify reality data background transparency using the Viewport API
   public static setRealityDataTransparency(crmProp: ContextRealityModelProps, vp: ScreenViewport, transparency?: number) {
-    // For this example we want to affect the appearance of *all* reality models.
     if (transparency === undefined)
       transparency = 0;
     // START APPEARANCE
--- src/frontend-samples/reality-data-sample/RealityDataWidget.tsx
+++ src/frontend-samples/reality-data-sample/RealityDataWidget.tsx
@@ -17,9 +17,9 @@ const RealityDataWidget: React.FunctionComponent = () => {
   // START STATE
   const showRealityDataState = React.useRef<Map<string, boolean>>(new Map());
   const transparencyRealityDataState = React.useRef<Map<string, number>>(new Map());
-  // const [realityDataTransparencyState, setRealityDataTransparencyState] = React.useState<Map<string, number>>();
   const [availableRealityModels, setAvailableRealityModels] = React.useState<ContextRealityModelProps[]>();
-  const [updateState, setUpdateState] = React.useState<string>("");
+  const [updateAttachedState, setUpdateAttachedState] = React.useState<string>("");
+  const [updateTransparencyState, setUpdateTransparencyState] = React.useState<string>("");
 
   // END STATE
 
@@ -27,94 +27,57 @@ const RealityDataWidget: React.FunctionComponent = () => {
   // Initialize the widget
   useEffect(() => {
     IModelApp.viewManager.onViewOpen.addOnce(async (_vp: ScreenViewport) => {
-      const initialStateTransparency = new Map();
       const availRealityModels = await RealityDataApi.getRealityModels(_vp.iModel);
       setAvailableRealityModels(availRealityModels);
       for (const model of availRealityModels) {
         showRealityDataState.current.set(model.tilesetUrl, true);
-        initialStateTransparency.set(model.tilesetUrl, 50);
         RealityDataApi.toggleRealityModel(model, _vp, true);
         RealityDataApi.setRealityDataTransparency(model, _vp, 0);
       }
-      // setRealityDataTransparencyState(initialStateTransparency);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   // END INITIAL_STATE
 
-  // START TRANSPARENCY_HOOK
-  // When just the transparency bar is changed, only call update transparency
-  // useEffect(() => {
-  //  const vp = IModelApp.viewManager.selectedView;
-  //  if (vp) {
-  //    RealityDataApi.setRealityDataTransparency(vp, realityDataTransparencyState)
-  //      .catch((error) => {
-  //        // eslint-disable-next-line no-console
-  //        console.error(error);
-  //      });
-  //  }
-  // }, [realityDataTransparencyState]);
-  // END TRANSPARENCY_HOOK
-
-  // START REALITY_TOGGLE
-  // Create the react components for the toggle
-  // const createToggle = (label: string, info: string) => {
-  //   const element = <Toggle isOn={showRealityDataState?.get("")} onChange={async (checked: boolean) => setShowRealityDataState(checked)} />;
-  //   return (
-  //     <>
-  //       <span><span style={{ marginRight: "1em" }} className="icon icon-help" title={info}></span>{label}</span>
-  //       {element}
-  //     </>
-  //   );
-  // };
-  // const createRealityModelItem = (realityDataContext: ContextRealityModelProps) => {
-  //   const elem = <Toggle isOn={true} onChange={async (checked: boolean) => setShowRealityDataState(checked)} />;
-  //   return (
-  //     <>
-  //       <span>{realityDataContext.name}</span>
-  //       {elem}
-  //     </>
-  //   );
-  // };
-  // END REALITY_TOGGLE
-
-  // START TRANSPARENCY_SLIDER
-  // Create the react component for the transparency slider
-  // const createTransparencySlider = (label: string, info: string) => {
-  //   const element = <input type={"range"} min={0} max={99} defaultValue={0} onChange={(event: React.ChangeEvent<HTMLInputElement>) => setRealityDataTransparencyState(Math.abs(Number(event.target.value) / 100))} />;
-  //   return (
-  //     <>
-  //       <span><span style={{ marginRight: "1em" }} className="icon icon-help" title={info}></span>{label}</span>
-  //       {element}
-  //     </>
-  //   );
-  // };
-  // END TRANSPARENCY_SLIDER
-
   // START REALITY_HOOK
-  // When the button is toggled, display the realityModel and set its transparency to where the slider is currently at.
+  // When the button is toggled, display the realityModel and set its transparency to where the slider is currently set.
   useEffect(() => {
-    if (iModelConnection && updateState) {
+    if (iModelConnection && updateAttachedState) {
       const vp = IModelApp.viewManager.selectedView;
       if (vp && availableRealityModels && showRealityDataState) {
-        const model = availableRealityModels.find((x) => x.tilesetUrl === updateState);
-        if (model)
+        const model = availableRealityModels.find((x) => x.tilesetUrl === updateAttachedState);
+        if (model) {
           RealityDataApi.toggleRealityModel(model, vp, showRealityDataState.current.get(model.tilesetUrl));
+          RealityDataApi.setRealityDataTransparency(model, vp, transparencyRealityDataState.current.get(model.tilesetUrl));
+        }
       }
     }
-    setUpdateState("");
+    setUpdateAttachedState("");
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [showRealityDataState, updateState]);
+  }, [showRealityDataState, updateAttachedState]);
   // END REALITY_HOOK
+
+  useEffect(() => {
+    if (iModelConnection && updateTransparencyState) {
+      const vp = IModelApp.viewManager.selectedView;
+      if (vp && availableRealityModels && showRealityDataState) {
+        const model = availableRealityModels.find((x) => x.tilesetUrl === updateTransparencyState);
+        if (model)
+          RealityDataApi.setRealityDataTransparency(model, vp, transparencyRealityDataState.current.get(model.tilesetUrl));
+      }
+    }
+    setUpdateTransparencyState("");
+  }, [transparencyRealityDataState, updateTransparencyState]);
 
   const updateShowRealityDataState = (url: string, checked: boolean) => {
     showRealityDataState.current.set(url, checked);
-    setUpdateState(url);
+    setUpdateAttachedState(url);
   };
 
-  // const updateRealityDataTransparencyState = (url: string, val: number) => {
-    // transparencyRealityDataState.current.set(url, val);
-  // };
+  const updateRealityDataTransparencyState = (url: string, val: number) => {
+    transparencyRealityDataState.current.set(url, val);
+    setUpdateTransparencyState(url);
+  };
 
   // START WIDGET_UI
   return (
@@ -126,8 +89,12 @@ const RealityDataWidget: React.FunctionComponent = () => {
               <>
                 <span>{element.name}</span>
                 <Toggle key={element.tilesetUrl} isOn={true} onChange={async (checked: boolean) => updateShowRealityDataState(element.tilesetUrl, checked)} />
-                <span><span style={{ marginRight: "1em" }} className="icon icon-help" title={"Transparency"}></span>{"Adjusting this slider."}</span>
-                {/* <input type={"range"} min={0} max={99} defaultValue={0} onChange={(event: React.ChangeEvent<HTMLInputElement>) => updateRealityDataTransparencyState(element.tilesetUrl, Math.abs(Number(event.target.value) / 100))} /> */}
+
+                {element.orbitGtBlob === undefined &&
+                  <>
+                    <span><span style={{ marginRight: "1em" }} className="icon icon-help" title={"This slider adjusts the transparency of the reality model."}></span>{"Transparency"}</span>
+                    <input type={"range"} min={0} max={99} defaultValue={0} onChange={(event: React.ChangeEvent<HTMLInputElement>) => updateRealityDataTransparencyState(element.tilesetUrl, Math.abs(Number(event.target.value) / 100))} />
+                  </>}
               </>);
           })
           }
