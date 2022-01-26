92f4b9720d821f1cf041ba58be65c96f03fa07fb
Stefan Retief <stefan.retief@me.com>
2022-01-26 12:23:31 -0800

    upgraded IoT App

--- src/frontend-samples/iot-alert-sample/IotAlertApp.tsx
+++ src/frontend-samples/iot-alert-sample/IotAlertApp.tsx
@@ -2,12 +2,11 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import { AuthorizationClient, default3DSandboxUi, mapLayerOptions, SampleIModels, useSampleWidget, ViewSetup } from "@itwin/sandbox";
+import { AuthorizationClient, mapLayerOptions, SampleIModels, useSampleWidget, ViewSetup } from "@itwin/sandbox";
 import React, { FunctionComponent } from "react";
 import { Viewer } from "@itwin/web-viewer-react";
 import { IModelConnection } from "@itwin/core-frontend";
 import { IotAlertWidgetProvider } from "./IotAlertWidget";
-import { MessageRenderer } from "@itwin/core-react";
 
 const uiProviders = [new IotAlertWidgetProvider()];
 
@@ -21,7 +20,6 @@ const IotAlertApp: FunctionComponent = () => {
   /** The sample's render method */
   return (
     <>
-      <MessageRenderer message={""} />
       { /** Viewport to display the iModel */}
       {sampleIModelInfo?.iModelName && sampleIModelInfo?.contextId && sampleIModelInfo?.iModelId &&
         <Viewer
@@ -31,7 +29,10 @@ const IotAlertApp: FunctionComponent = () => {
           enablePerformanceMonitors={true}
           viewportOptions={{ viewState: _initialViewstate }}
           mapLayerOptions={mapLayerOptions}
-          defaultUiConfig={default3DSandboxUi}
+          defaultUiConfig={{
+            hidePropertyGrid: true,
+            hideTreeView: true,
+          }}
           theme="dark"
           uiProviders={uiProviders}
         />
