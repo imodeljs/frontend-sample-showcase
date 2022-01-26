408dae658809643d2f227237b6510c6d5a90a875
Stefan Retief <stefan.retief@me.com>
2022-01-26 09:15:33 -0800

    moved sandbox package to @itwin/sandbox

---
+++ src/@itwin/sandbox/view/MapLayerOptions.ts
@@ -0,0 +1,12 @@
+import { MapLayerOptions } from "@itwin/core-frontend";
+
+export const mapLayerOptions: MapLayerOptions = {
+  BingMaps: {
+    key: "key",
+    value: "AtaeI3QDNG7Bpv1L53cSfDBgBKXIgLq3q-xmn_Y2UyzvF-68rdVxwAuje49syGZt",
+  },
+  MapBoxImagery: {
+    key: "MapBoxKey",
+    value: "pk%2EeyJ1IjoibWFwYm94YmVudGxleSIsImEiOiJjaWZvN2xpcW00ZWN2czZrcXdreGg2eTJ0In0%2Ef7c9GAxz6j10kZvL%5F2DBHg",
+  },
+};
