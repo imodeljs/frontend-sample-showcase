965b23e6d7ccff01281698d6db1326e28cb66a44
tomdicarlo <24437642+tomdicarlo@users.noreply.github.com>
2022-01-26 13:31:37 -0700

    Added cesium key to MapLayerOptions

--- src/@itwin/sandbox/view/MapLayerOptions.ts
+++ src/@itwin/sandbox/view/MapLayerOptions.ts
@@ -2,7 +2,7 @@ import { MapLayerOptions } from "@itwin/core-frontend";
 
 export const mapLayerOptions: MapLayerOptions = {
   BingMaps: {
-    key: "key",
+    key: "BingKey",
     value: "AtaeI3QDNG7Bpv1L53cSfDBgBKXIgLq3q-xmn_Y2UyzvF-68rdVxwAuje49syGZt",
   },
   MapBoxImagery: {
@@ -10,3 +10,5 @@ export const mapLayerOptions: MapLayerOptions = {
     value: "pk%2EeyJ1IjoibWFwYm94YmVudGxleSIsImEiOiJjaWZvN2xpcW00ZWN2czZrcXdreGg2eTJ0In0%2Ef7c9GAxz6j10kZvL%5F2DBHg",
   },
 };
+
+export const bentleyCesiumKey: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkZWIxNzk1OC0wNmVjLTQ1NDItOTBlYS1lOTViMDljNzQyNWUiLCJpZCI6MTQwLCJzY29wZXMiOlsiYXNsIiwiYXNyIiwiYXN3IiwiZ2MiXSwiaWF0IjoxNTYyMDA0NTYwfQ.VyMP5TPl--eX2bCQjIY7ijfPCd-J0sSPnEFj_mfPC3k";
