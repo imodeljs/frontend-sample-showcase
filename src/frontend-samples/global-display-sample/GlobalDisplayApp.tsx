2314d83d2fbb3f34ede888f538d04b817adb81ed
tomdicarlo <24437642+tomdicarlo@users.noreply.github.com>
2022-01-27 12:14:16 -0700

    Added cesium key to samples using map data

--- src/frontend-samples/global-display-sample/GlobalDisplayApp.tsx
+++ src/frontend-samples/global-display-sample/GlobalDisplayApp.tsx
@@ -9,6 +9,7 @@ import { Viewer } from "@itwin/web-viewer-react";
 import { GlobalDisplayWidgetProvider } from "./GlobalDisplayWidget";
 import { GlobalDisplayApi } from "./GlobalDisplayApi";
 import { MessageRenderer } from "@itwin/core-react";
+import { bentleyCesiumKey } from "@itwin/sandbox/view/MapLayerOptions";
 
 const uiProviders = [new GlobalDisplayWidgetProvider()];
 
@@ -39,6 +40,7 @@ const GlobalDisplayApp: FunctionComponent = () => {
           authClient={AuthorizationClient.oidcClient}
           enablePerformanceMonitors={true}
           viewportOptions={{ viewState: _initialViewstate }}
+          tileAdminOptions={{ cesiumIonKey: bentleyCesiumKey }}
           defaultUiConfig={default3DSandboxUi}
           mapLayerOptions={mapLayerOptions}
           uiProviders={uiProviders}
